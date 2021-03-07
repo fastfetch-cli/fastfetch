6cc99423c93e87b8f016f35d6b01c20dafb7470a
Linus Dierheimer <Linus@Dierheimer.de>
2021-03-07 20:05:00 +0100

    performance tests

---
+++ tests/performance.c
@@ -0,0 +1,58 @@
+#include "fastfetch.h"
+
+#include <time.h>
+
+#define FASTFETCH_TEST_PERFORMANCE(func) \
+    { \
+        struct timespec start, end; \
+        clock_gettime(CLOCK_REALTIME, &start); \
+        func; \
+        clock_gettime(CLOCK_REALTIME, &end); \
+        long nanos = end.tv_nsec - start.tv_nsec; \
+        printf("\033[F"); \
+        for(uint8_t i = 0; i < 110; i++) printf("\033[C"); \
+        printf("%08ldns\n", nanos); \
+    } \
+
+int main(int argc, char** argv)
+{
+    FFinstance instance;
+    
+    FASTFETCH_TEST_PERFORMANCE(
+        puts("Initialization");
+        ffInitState(&instance.state);
+        ffDefaultConfig(&instance.config);
+    )
+
+    FASTFETCH_TEST_PERFORMANCE(
+        puts("Configuration");
+        ffLoadLogoSet(&instance.config, "arch");
+        strcpy(instance.config.color, instance.config.logo.color);
+    )
+
+    FASTFETCH_TEST_PERFORMANCE(ffPrintTitle(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintSeperator(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintOS(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintHost(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintKernel(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintUptime(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintPackages(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintShell(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintResolution(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintDesktopEnvironment(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintWM(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintTheme(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintIcons(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintFont(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintTerminal(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintCPU(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintGPU(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintMemory(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintDisk(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintBattery(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintLocale(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintBreak(&instance))
+    FASTFETCH_TEST_PERFORMANCE(ffPrintColors(&instance))
+
+    return 0;
+}
---
+++ tests/performance.sh
@@ -0,0 +1,6 @@
+rm -rf tests/build/
+mkdir -p tests/build/
+cd tests/build/
+cmake ../.. -DBUILD_TESTS=ON
+cmake --build . --target fastfetch-test-performance
+./fastfetch-test-performance "$@"
