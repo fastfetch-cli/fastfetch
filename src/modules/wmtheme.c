a7b81f0a2e286c7c99bf90b1cff2d6773b3c35e8
DarN <82701459+DarNCelsius@users.noreply.github.com>
2021-04-25 01:09:46 +0200

    Added WM Theme support for LXQt, LXDE and Openbox

--- src/modules/wmtheme.c
+++ src/modules/wmtheme.c
@@ -1,4 +1,6 @@
 #include "fastfetch.h"
+#include "string.h"
+#include "libxml/parser.h"
 
 #define FF_WMTHEME_MODULE_NAME "WM Theme"
 #define FF_WMTHEME_NUM_FORMAT_ARGS 1
@@ -33,6 +35,54 @@ static void printKWin(FFinstance* instance)
     printWMTheme(instance, theme);
 }
 
+xmlNode* ffXmlFindNode(xmlNode* node, const xmlChar* name)
+{
+    while (node) {
+        if (!xmlStrcmp(node->name, (const xmlChar*) name))
+            return node;
+        node = node->next;
+    }
+    return NULL;
+}
+
+static void printOpenbox(FFinstance* instance)
+{
+	xmlDoc* document;
+	xmlNode* node;
+	char fullPath[64];
+	char theme[256];
+	theme[0] = '\0';
+
+	strcpy(fullPath, instance->state.passwd->pw_dir);
+	// Is there a better way to get deName internally?
+	const char* deName = getenv("XDG_SESSION_DESKTOP");
+
+	if (strcmp(deName, (const char*)"LXQt Desktop") == 0)
+		strcat(fullPath, "/.config/openbox/lxqt-rc.xml");
+	else if(strcmp(deName, (const char*)"LXDE") == 0)
+		strcat(fullPath, "/.config/openbox/lxde-rc.xml");
+	else 
+		strcat(fullPath, "/.config/openbox/rc.xml");
+
+	document = xmlReadFile(fullPath, NULL, XML_PARSE_SAX1 | XML_PARSE_NOBLANKS | XML_PARSE_RECOVER);
+	node = xmlDocGetRootElement(document)->children;
+
+	if ((node = ffXmlFindNode(node, (const xmlChar*)"theme")) != NULL)
+	{
+		if ((node = ffXmlFindNode(node->xmlChildrenNode, (const xmlChar*)"name")) != NULL)
+			strcpy(theme, (char*)xmlNodeGetContent(node));
+	}
+	xmlFreeDoc(document);
+
+	if(theme[0] == '\0')
+	{
+		ffPrintError(instance, FF_WMTHEME_MODULE_NAME, 0, &instance->config.wmThemeKey, &instance->config.wmThemeFormat, FF_WMTHEME_NUM_FORMAT_ARGS, "Couldn't find theme name in \"%s\"", fullPath);
+		return;
+	}
+
+	printWMTheme(instance, theme);
+}
+
 void ffPrintWMTheme(FFinstance* instance)
 {
     const FFWMResult* result = ffCalculateWM(instance);
@@ -45,6 +95,8 @@ void ffPrintWMTheme(FFinstance* instance)
 
     if(ffStrbufIgnCaseCompS(&result->prettyName, "KWin") == 0)
         printKWin(instance);
+    else if(ffStrbufIgnCaseCompS(&result->prettyName, "Openbox") == 0)
+        printOpenbox(instance);
     else
         ffPrintError(instance, FF_WMTHEME_MODULE_NAME, 0, &instance->config.wmThemeKey, &instance->config.wmThemeFormat, FF_WMTHEME_NUM_FORMAT_ARGS, "Unknown WM: %s", result->prettyName.chars);
 }
