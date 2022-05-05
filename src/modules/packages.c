36c2ce621995635abc6fd13d5c24752a3fbf2210
Linus Dierheimer <Linus@Dierheimer.de>
2022-05-05 15:48:01 +0200

    Look for newer versions of librpm

--- src/modules/packages.c
+++ src/modules/packages.c
@@ -14,7 +14,7 @@
 
 static uint32_t getRpmPackageCount(FFinstance* instance)
 {
-    FF_LIBRARY_LOAD(rpm, instance->config.librpm, 0, "librpm.so", 5)
+    FF_LIBRARY_LOAD(rpm, instance->config.librpm, 0, "librpm.so", 12)
     FF_LIBRARY_LOAD_SYMBOL(rpm, rpmReadConfigFiles, 0)
     FF_LIBRARY_LOAD_SYMBOL(rpm, rpmtsCreate, 0)
     FF_LIBRARY_LOAD_SYMBOL(rpm, rpmtsInitIterator, 0)
