f362fdb01d80cf1fbadf28a48e090fe32cb30b68
李通洲 <carter.li@eoitek.com>
2022-09-16 11:03:46 +0800

    Battery: move detection code into `src/detection`
    Prepare for macOS implementation

---
+++ src/detection/battery/battery_apple.c
@@ -0,0 +1,7 @@
+#include "fastfetch.h"
+#include "battery.h"
+
+const char* ffDetectBatteryImpl(FFinstance* instance, FFlist* results) {
+    FF_UNUSED(instance, results);
+    return "Unimplemented";
+}
