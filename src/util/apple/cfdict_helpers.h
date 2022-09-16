d46f9ab4dcfddcdd81251fad94ccd67fa3305316
Carter Li <zhangsongcui3371@sina.com>
2022-09-17 02:17:27 +0800

    util/cfdict_helpers: code improvements
    1. simplify CFString usage
    2. support get string from CFDataRef

--- src/util/apple/cfdict_helpers.h
+++ src/util/apple/cfdict_helpers.h
@@ -6,9 +6,8 @@
 #include "fastfetch.h"
 #include <CoreFoundation/CoreFoundation.h>
 
-const void* ffCfDictGetValue(CFMutableDictionaryRef dict, const char* str);
-bool ffCfDictGetString(CFMutableDictionaryRef dict, const char* key, FFstrbuf* result);
-bool ffCfDictGetBool(CFMutableDictionaryRef dict, const char* key, bool* result);
-bool ffCfDictGetInt(CFMutableDictionaryRef dict, const char* key, int* result);
+bool ffCfDictGetString(CFMutableDictionaryRef dict, CFStringRef key, FFstrbuf* result);
+bool ffCfDictGetBool(CFMutableDictionaryRef dict, CFStringRef key, bool* result);
+bool ffCfDictGetInt(CFMutableDictionaryRef dict, CFStringRef key, int* result);
 
 #endif
