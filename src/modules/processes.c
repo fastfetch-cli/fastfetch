c64e2c14c2b83427dac44703be8b69e933c659ad
Carter Li <zhangsongcui3371@sina.com>
2022-09-17 11:21:16 +0800

    Processes: add macOS support

--- src/modules/processes.c
+++ src/modules/processes.c
@@ -4,12 +4,24 @@
 #define FF_PROCESSES_MODULE_NAME "Processes"
 #define FF_PROCESSES_NUM_FORMAT_ARGS 1
 
+#ifdef __APPLE__
+    #include <sys/sysctl.h>
+#endif
+
 void ffPrintProcesses(FFinstance* instance)
 {
     #if FF_HAVE_SYSINFO_H
         uint16_t numProcesses = instance->state.sysinfo.procs;
     #else
-        uint16_t numProcesses = 0;
+        int request[] = {CTL_KERN, KERN_PROC, KERN_PROC_ALL};
+        size_t length;
+
+        if(sysctl(request, sizeof(request) / sizeof(*request), NULL, &length, NULL, 0) != 0)
+        {
+            ffPrintError(instance, FF_PROCESSES_MODULE_NAME, 0, &instance->config.processes, "sysctl() failed");
+            return;
+        }
+        uint16_t numProcesses = (uint16_t)(length / sizeof(struct kinfo_proc));
     #endif
 
     if(instance->config.processes.outputFormat.length == 0)
