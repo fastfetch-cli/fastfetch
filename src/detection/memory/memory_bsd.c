c7525efe38fe28d560406f39193003514b7e61e7
Yonas Yanfa <yonas.y@gmail.com>
2022-09-26 00:47:50 -0400

    Even better BSD support.

--- src/detection/memory/memory_bsd.c
+++ src/detection/memory/memory_bsd.c
@@ -1,17 +1,17 @@
 #include "memory.h"
 #include "common/sysctl.h"
 
-static void detectRam(FFMemoryStorage* ram)
+static void detectRam(FFMemoryStorage* ram, FFMemoryResult* memory)
 {
     uint32_t pageSize = (uint32_t) ffSysctlGetInt("hw.pagesize", 0);
-    if(pageSize == 0)
+    if (pageSize == 0)
     {
         ffStrbufAppendS(&ram->error, "Failed to read hw.pagesize");
         return;
     }
 
     memory->ram.bytesTotal = (uint64_t) ffSysctlGetInt64("vm.stats.vm.v_page_count", 0) * pageSize;
-    if(ram->bytesTotal == 0)
+    if (ram->bytesTotal == 0)
     {
         ffStrbufAppendS(&ram->error, "Failed to read vm.stats.vm.v_page_count");
         return;
@@ -27,8 +27,9 @@ static void detectSwap(FFMemoryStorage* swap)
 {
     ffStrbufAppendS(&swap->error, "Not implemented");
 }
+
 void ffDetectMemoryImpl(FFMemoryResult* memory)
 {
-    detectRam(&memory->ram);
+    detectRam(&memory->ram, memory);
     detectSwap(&memory->swap);
 }
