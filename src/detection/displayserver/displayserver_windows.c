150b033415b3418cfb362aacfda629e56741771f
李通洲 <carter.li@eoitek.com>
2022-10-05 19:52:16 +0800

    Resolution: add support for Windows

--- src/detection/displayserver/displayserver_windows.c
+++ src/detection/displayserver/displayserver_windows.c
@@ -1,14 +1,41 @@
 #include "displayserver.h"
 
+#include <dwmapi.h>
+#include <WinUser.h>
+
 void ffConnectDisplayServerImpl(FFDisplayServerResult* ds, const FFinstance* instance)
 {
     FF_UNUSED(instance);
 
-    ffStrbufInitA(&ds->wmProcessName, 0);
-    ffStrbufInitA(&ds->wmPrettyName, 0);
-    ffStrbufInitA(&ds->wmProtocolName, 0);
-    ffStrbufInitA(&ds->deProcessName, 0);
-    ffStrbufInitA(&ds->dePrettyName, 0);
-    ffStrbufInitA(&ds->deVersion, 0);
-    ffListInitA(&ds->resolutions, sizeof(FFResolutionResult), 0);
+    BOOL enabled;
+    if(SUCCEEDED(DwmIsCompositionEnabled(&enabled)) && enabled == TRUE)
+    {
+        ffStrbufInitS(&ds->wmProcessName, "dwm.exe");
+        ffStrbufInitS(&ds->wmPrettyName, "Desktop Window Manager");
+    }
+    else
+    {
+        ffStrbufInitS(&ds->wmProcessName, "internal");
+        ffStrbufInitS(&ds->wmPrettyName, "internal");
+    }
+    ffStrbufInit(&ds->wmProtocolName);
+    ffStrbufInit(&ds->deProcessName);
+    ffStrbufInit(&ds->dePrettyName);
+    ffStrbufInit(&ds->deVersion);
+    ffListInit(&ds->resolutions, sizeof(FFResolutionResult));
+
+    DISPLAY_DEVICEW displayDevice = { .cb = sizeof(DISPLAY_DEVICEW) };
+    for(DWORD devNum = 0; EnumDisplayDevicesW(NULL, devNum, &displayDevice, 0) != 0; ++devNum)
+    {
+        if(!(displayDevice.StateFlags & DISPLAY_DEVICE_ACTIVE))
+            continue;
+        DEVMODEW devMode = { .dmSize = sizeof(DEVMODEW) };
+        if(EnumDisplaySettingsW(displayDevice.DeviceName, ENUM_CURRENT_SETTINGS, &devMode) == 0)
+            continue;
+
+        FFResolutionResult* resolution = (FFResolutionResult*) ffListAdd(&ds->resolutions);
+        resolution->width = devMode.dmPelsWidth;
+        resolution->height = devMode.dmPelsHeight;
+        resolution->refreshRate = devMode.dmDisplayFrequency;
+    }
 }
