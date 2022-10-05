8ea2014baf32480ce2073e90725021dd8c5c0795
李通洲 <carter.li@eoitek.com>
2022-10-05 23:51:59 +0800

    OpenGL: add support for Windows

---
+++ src/detection/opengl/opengl.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef FF_INCLUDED_detection_opengl_opengl
+#define FF_INCLUDED_detection_opengl_opengl
+
+#include "fastfetch.h"
+
+typedef struct FFOpenGLResult
+{
+    FFstrbuf version;
+    FFstrbuf renderer;
+    FFstrbuf vendor;
+    FFstrbuf slv;
+} FFOpenGLResult;
+
+const char* ffDetectOpenGL(FFinstance* instance, FFOpenGLResult* result);
+
+#endif
