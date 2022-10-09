6a9fd7a84d20dd195ac71b085f038ffb887f0b0d
李通洲 <carter.li@eoitek.com>
2022-10-09 21:12:27 +0800

    TerminalShell: silence compiler warnings

--- src/detection/terminalshell/terminalshell_windows.cpp
+++ src/detection/terminalshell/terminalshell_windows.cpp
@@ -52,7 +52,7 @@ static bool getProcessInfo(uint32_t pid, uint32_t* ppid, FFstrbuf* pname, FFstrb
 
 static void getShellVersion(FFstrbuf* exe, FFstrbuf* version)
 {
-    char* const argv[] = { exe->chars, "--version", NULL };
+    char* const argv[] = { exe->chars, (char*)"--version", NULL };
     ffProcessAppendStdOut(version, argv);
     ffStrbufTrimRight(version, '\n');
     ffStrbufSubstrAfterLastC(version, ' ');
