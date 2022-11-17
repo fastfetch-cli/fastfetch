a498e9e14230c09c00d1f89409f609fa551c1e4a
李通洲 <carter.li@eoitek.com>
2022-11-17 10:33:50 +0800

    CPU: don't remove `(R)` in CPU name to be consistant with GPU name

--- src/detection/cpu/cpu.c
+++ src/detection/cpu/cpu.c
@@ -31,7 +31,6 @@ static void detectCPU(const FFinstance* instance, FFCPUResult* cpu)
         return;
 
     const char* removeStrings[] = {
-        "(R)", "(r)", "(TM)", "(tm)",
         " CPU", " FPU", " APU", " Processor",
         " Dual-Core", " Quad-Core", " Six-Core", " Eight-Core", " Ten-Core",
         " 2-Core", " 4-Core", " 6-Core", " 8-Core", " 10-Core", " 12-Core", " 14-Core", " 16-Core",
