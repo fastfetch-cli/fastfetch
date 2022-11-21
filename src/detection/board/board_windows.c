5aebb400dabef5cbe5f2cd0b0ec8692a1af4bb94
李通洲 <carter.li@eoitek.com>
2022-11-21 17:34:54 +0800

    Board: improve performance (Windows)

---
+++ src/detection/board/board_windows.c
@@ -0,0 +1,21 @@
+#include "board.h"
+#include "util/windows/register.h"
+
+void ffDetectBoard(FFBoardResult* board)
+{
+    ffStrbufInit(&board->error);
+
+    ffStrbufInit(&board->boardName);
+    ffStrbufInit(&board->boardVendor);
+    ffStrbufInit(&board->boardVersion);
+
+    FF_HKEY_AUTO_DESTROY hKey = NULL;
+
+    if(!ffRegOpenKeyForRead(HKEY_LOCAL_MACHINE, "HARDWARE\\DESCRIPTION\\System\\BIOS", &hKey, &board->error))
+        return;
+
+    if(!ffRegReadStrbuf(hKey, "BaseBoardProduct", &board->boardName, &board->error))
+        return;
+    ffRegReadStrbuf(hKey, "BaseBoardManufacturer", &board->boardVendor, NULL);
+    ffRegReadStrbuf(hKey, "BaseBoardVersion", &board->boardVersion, NULL);
+}
