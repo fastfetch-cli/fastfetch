6a304dbb4d098f7f67dbd8d6c30b237c6e9b20f1
李通洲 <carter.li@eoitek.com>
2022-11-21 18:51:46 +0800

    OS: improve performance (Windows)

--- src/detection/os/os_windows.cpp
+++ src/detection/os/os_windows.cpp
@@ -3,6 +3,50 @@ extern "C" {
 }
 #include "util/windows/wmi.hpp"
 
+static const char* getOsNameByWmi(FFstrbuf* osName)
+{
+    FFWmiQuery query(L"SELECT Caption FROM Win32_OperatingSystem");
+    if(!query)
+        return "Query WMI service failed";
+
+    if(FFWmiRecord record = query.next())
+    {
+        record.getString(L"Caption", osName);
+        ffStrbufTrimRight(osName, ' ');
+        return NULL;
+    }
+
+    return "No WMI result returned";
+}
+
+static inline void wrapFreeLibrary(HMODULE* module)
+{
+    if(*module)
+        FreeLibrary(*module);
+}
+
+static const char* getOsNameByWinbrand(FFstrbuf* osName)
+{
+    //https://dennisbabkin.com/blog/?t=how-to-tell-the-real-version-of-windows-your-app-is-running-on#ver_string
+    if(HMODULE __attribute__((__cleanup__(wrapFreeLibrary))) hWinbrand = LoadLibraryW(L"winbrand.dll"))
+    {
+        PWSTR(WINAPI* BrandingFormatString)(PCWSTR);
+        (FARPROC&)BrandingFormatString = GetProcAddress(hWinbrand, "BrandingFormatString");
+        if(!BrandingFormatString)
+            return "GetProcAddress(BrandingFormatString) failed";
+
+        const wchar_t* rawName = BrandingFormatString(L"%WINDOWS_LONG%");
+        int size_needed = WideCharToMultiByte(CP_UTF8, 0, rawName, -1, nullptr, 0, nullptr, nullptr);
+        ffStrbufEnsureFree(osName, (uint32_t)size_needed);
+        WideCharToMultiByte(CP_UTF8, 0, rawName, -1, osName->chars, size_needed, nullptr, nullptr);
+        osName->length = (uint32_t)size_needed;
+        osName->chars[size_needed] = '\0';
+        GlobalFree((HGLOBAL)rawName);
+        return NULL;
+    }
+    return "LoadLibraryW(winbrand.dll) failed";
+}
+
 extern "C"
 void ffDetectOSImpl(FFOSResult* os, const FFinstance* instance)
 {
@@ -19,56 +63,54 @@ void ffDetectOSImpl(FFOSResult* os, const FFinstance* instance)
     ffStrbufInit(&os->systemName);
     ffStrbufInit(&os->architecture);
 
-    FFWmiQuery query(L"SELECT Caption, Version, BuildNumber, OSArchitecture FROM Win32_OperatingSystem");
-    if(!query)
+    if(getOsNameByWinbrand(&os->variant) && getOsNameByWmi(&os->variant))
         return;
 
-    if(FFWmiRecord record = query.next())
+    ffStrbufTrimRight(&os->variant, ' ');
+
+    //WMI returns the "Microsoft" prefix while BrandingFormatString doesn't. Make them consistant.
+    if(ffStrbufStartsWithS(&os->variant, "Microsoft "))
+        ffStrbufSubstrAfter(&os->variant, strlen("Microsoft ") - 1);
+
+    if(ffStrbufStartsWithS(&os->variant, "Windows "))
     {
-        record.getString(L"Caption", &os->variant);
-        ffStrbufTrimRight(&os->variant, ' ');
-        if(ffStrbufStartsWithS(&os->variant, "Microsoft Windows "))
+        ffStrbufAppendS(&os->name, "Windows");
+        ffStrbufAppendS(&os->prettyName, "Windows");
+
+        ffStrbufSubstrAfter(&os->variant, strlen("Windows ") - 1);
+
+        if(ffStrbufStartsWithS(&os->variant, "Server "))
         {
-            ffStrbufAppendS(&os->name, "Microsoft Windows");
-            ffStrbufAppendS(&os->prettyName, "Windows");
-
-            ffStrbufSubstrAfter(&os->variant, strlen("Microsoft Windows ") - 1);
-
-            if(ffStrbufStartsWithS(&os->variant, "Server "))
-            {
-                ffStrbufAppendS(&os->name, " Server");
-                ffStrbufAppendS(&os->prettyName, " Server");
-                ffStrbufSubstrAfter(&os->variant, strlen(" Server") - 1);
-            }
-
-            uint32_t index = ffStrbufFirstIndexC(&os->variant, ' ');
-            ffStrbufAppendNS(&os->version, index, os->variant.chars);
-            ffStrbufSubstrAfter(&os->variant, index);
-
-            // Windows Server 20xx Rx
-            if(ffStrbufEndsWithC(&os->prettyName, 'r'))
-            {
-                if(os->variant.chars[0] == 'R' &&
-                isdigit(os->variant.chars[1]) &&
-                (os->variant.chars[2] == '\0' || os->variant.chars[2] == ' '))
-                {
-                    ffStrbufAppendF(&os->version, " R%c", os->variant.chars[1]);
-                    ffStrbufSubstrAfter(&os->variant, strlen("Rx ") - 1);
-                }
-            }
-        }
-        else
-        {
-            // Unknown Windows name, please report this
-            ffStrbufAppend(&os->name, &os->variant);
-            ffStrbufClear(&os->variant);
+            ffStrbufAppendS(&os->name, " Server");
+            ffStrbufAppendS(&os->prettyName, " Server");
+            ffStrbufSubstrAfter(&os->variant, strlen(" Server") - 1);
         }
 
-        ffStrbufAppendF(&os->id, "%*s %*s", os->prettyName.length, os->prettyName.chars, os->version.length, os->version.chars);
+        uint32_t index = ffStrbufFirstIndexC(&os->variant, ' ');
+        ffStrbufAppendNS(&os->version, index, os->variant.chars);
+        ffStrbufSubstrAfter(&os->variant, index);
 
-        record.getString(L"BuildNumber", &os->buildID);
-        record.getString(L"OSArchitecture", &os->architecture);
-
-        ffStrbufSetS(&os->systemName, instance->state.utsname.sysname);
+        // Windows Server 20xx Rx
+        if(ffStrbufEndsWithC(&os->prettyName, 'r'))
+        {
+            if(os->variant.chars[0] == 'R' &&
+            isdigit(os->variant.chars[1]) &&
+            (os->variant.chars[2] == '\0' || os->variant.chars[2] == ' '))
+            {
+                ffStrbufAppendF(&os->version, " R%c", os->variant.chars[1]);
+                ffStrbufSubstrAfter(&os->variant, strlen("Rx ") - 1);
+            }
+        }
     }
+    else
+    {
+        // Unknown Windows name, please report this
+        ffStrbufAppend(&os->name, &os->variant);
+        ffStrbufClear(&os->variant);
+    }
+
+    ffStrbufAppendF(&os->id, "%*s %*s", os->prettyName.length, os->prettyName.chars, os->version.length, os->version.chars);
+
+    ffStrbufSetS(&os->architecture, instance->state.utsname.machine);
+    ffStrbufSetS(&os->systemName, instance->state.utsname.sysname);
 }
