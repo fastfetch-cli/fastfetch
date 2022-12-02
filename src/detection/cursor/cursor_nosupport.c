911780ba3ca4eb115200fd71a9c311cd5722c66a
李通洲 <carter.li@eoitek.com>
2022-12-02 22:20:23 +0800

    Cursor: silence compiler warnings

--- src/detection/cursor/cursor_nosupport.c
+++ src/detection/cursor/cursor_nosupport.c
@@ -2,5 +2,6 @@
 
 void ffDetectCursor(const FFinstance* instance, FFCursorResult* result)
 {
+    FF_UNUSED(instance, result);
     ffStrbufInitS(&result->error, "Not supported on this platform");
 }
