72dcc5223d78caac4a94111b6772dd7baa6436a5
李通洲 <carter.li@eoitek.com>
2022-12-10 13:11:41 +0800

    Global: rename `--disable-stdout-buffer` to `--unbuffered`

--- src/fastfetch.c
+++ src/fastfetch.c
@@ -909,7 +909,7 @@ static void parseOption(FFinstance* instance, FFdata* data, const char* key, con
         instance->config.multithreading = optionParseBoolean(value);
     else if(strcasecmp(key, "--allow-slow-operations") == 0)
         instance->config.allowSlowOperations = optionParseBoolean(value);
-    else if(strcasecmp(key, "--disable-stdout-buffer") == 0)
+    else if(strcasecmp(key, "--unbuffered") == 0)
     {
         if(optionParseBoolean(value))
             setvbuf(stdout, NULL, _IONBF, 0);
