dd77ebe11e78cb7ee8eabce35a72ad9ec89beddc
Linus Dierheimer <Linus@Dierheimer.de>
2023-01-14 19:59:54 +0100

    Fix GitHub code scanning alert

--- src/util/FFlist.c
+++ src/util/FFlist.c
@@ -41,7 +41,7 @@ bool ffListShift(FFlist* list, void* result)
         return false;
 
     memcpy(result, list->data, list->elementSize);
-    memmove(list->data, list->data + list->elementSize, list->elementSize * (list->length - 1));
+    memmove(list->data, list->data + list->elementSize, (size_t) list->elementSize * (list->length - 1));
     --list->length;
     return true;
 }
