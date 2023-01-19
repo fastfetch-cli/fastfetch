d9e79a53768fb195dc563c2062fb7d5e4e008d0a
李通洲 <carter.li@eoitek.com>
2023-01-19 14:57:54 +0800

    Fastfetch: print more info with `-v`

--- src/fastfetch.c
+++ src/fastfetch.c
@@ -859,7 +859,35 @@ static void parseOption(FFinstance* instance, FFdata* data, const char* key, con
     }
     else if(strcasecmp(key, "-v") == 0 || strcasecmp(key, "--version") == 0)
     {
-        puts("fastfetch "FASTFETCH_PROJECT_VERSION""FASTFETCH_PROJECT_VERSION_TWEAK);
+        #ifndef NDEBUG
+            #define FF_BUILD_TYPE "-debug"
+        #else
+            #define FF_BUILD_TYPE
+        #endif
+
+        #if defined(__x86_64__) || defined(__x86_64) || defined(__amd64__) || defined(__amd64)
+            #define FF_ARCHITECTURE "x86_64"
+        #elif defined(__i386__) || defined(__i386) || defined(__i486__) || defined(__i486) || defined(__i586__) || defined(__i586) || defined(__i686__) || defined(__i686)
+            #define FF_ARCHITECTURE "i386"
+        #elif defined(__aarch64__)
+            #define FF_ARCHITECTURE "aarch64"
+        #elif defined(__arm__)
+            #define FF_ARCHITECTURE "arm"
+        #elif defined(__mips__)
+            #define FF_ARCHITECTURE "mips"
+        #elif defined(__powerpc__) || defined(__powerpc)
+            #define FF_ARCHITECTURE "powerpc"
+        #elif defined(__riscv__) || defined(__riscv)
+            #define FF_ARCHITECTURE "riscv"
+        #elif
+            #define FF_ARCHITECTURE "unknown"
+        #endif
+
+        puts("fastfetch " FASTFETCH_PROJECT_VERSION FASTFETCH_PROJECT_VERSION_TWEAK FF_BUILD_TYPE " (" FF_ARCHITECTURE ")");
+
+        #undef FF_ARCHITECTURE
+        #undef FF_BUILD_TYPE
+
         exit(0);
     }
     else if(strcasecmp(key, "--version-raw") == 0)
