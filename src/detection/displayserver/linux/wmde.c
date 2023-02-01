55da030e2a5fcbb6eb55652b9a3bb90b77f1ae9c
CSoellinger <christopher.soellinger@gmail.com>
2023-02-01 10:50:31 +0100

    fix: move mutter(muffin) detection to get FF_WM_PRETTY_MUFFIN back

--- src/detection/displayserver/linux/wmde.c
+++ src/detection/displayserver/linux/wmde.c
@@ -70,12 +70,12 @@ static void applyPrettyNameIfWM(FFDisplayServerResult* result, const char* proce
         strcasecmp(processName, "gnome-shell") == 0 ||
         strcasecmp(processName, "gnome shell") == 0 ||
         strcasecmp(processName, "gnome-session-binary") == 0 ||
-        strcasecmp(processName, "Mutter") == 0 ||
-        strcasecmp(processName, "Mutter (Muffin)") == 0
+        strcasecmp(processName, "Mutter") == 0
     ) ffStrbufSetS(&result->wmPrettyName, FF_WM_PRETTY_MUTTER);
     else if(
         strcasecmp(processName, "cinnamon-session") == 0 ||
-        strcasecmp(processName, "Muffin") == 0
+        strcasecmp(processName, "Muffin") == 0 ||
+        strcasecmp(processName, "Mutter (Muffin)") == 0
     ) ffStrbufSetS(&result->wmPrettyName, FF_WM_PRETTY_MUFFIN);
     else if(strcasecmp(processName, "sway") == 0)
         ffStrbufSetS(&result->wmPrettyName, FF_WM_PRETTY_SWAY);
