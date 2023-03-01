78cb2781e6af9b196f3c42b49f30c1417d55f1f2
李通洲 <carter.li@eoitek.com>
2023-03-01 15:40:18 +0800

    Brightness (macOS): display monitor names

--- src/detection/brightness/brightness_apple.c
+++ src/detection/brightness/brightness_apple.c
@@ -1,8 +1,10 @@
 #include "brightness.h"
+#include "util/apple/cf_helpers.h"
 
 #include <CoreGraphics/CoreGraphics.h>
 
 extern int DisplayServicesGetBrightness(CGDirectDisplayID display, float *brightness) __attribute__((weak_import));
+extern CFDictionaryRef CoreDisplay_DisplayCreateInfoDictionary(CGDirectDisplayID display) __attribute__((weak_import));
 
 const char* ffDetectBrightness(FFlist* result)
 {
@@ -22,11 +24,27 @@ const char* ffDetectBrightness(FFlist* result)
             continue;
 
         FFBrightnessResult* display = (FFBrightnessResult*) ffListAdd(result);
-        if(CGDisplayIsBuiltin(screen))
-            ffStrbufInitS(&display->name, "built-in");
-        else
-            ffStrbufInitF(&display->name, "external-%u", (unsigned) screen);
         display->value = brightness * 100;
+        ffStrbufInit(&display->name);
+
+        if(CoreDisplay_DisplayCreateInfoDictionary)
+        {
+            CFDictionaryRef FF_CFTYPE_AUTO_RELEASE displayInfo = CoreDisplay_DisplayCreateInfoDictionary(screen);
+            if(displayInfo)
+            {
+                CFDictionaryRef productNames;
+                if(!ffCfDictGetDict(displayInfo, CFSTR(kDisplayProductName), &productNames))
+                {
+                    if(!ffCfDictGetString(productNames, CFSTR("en_US"), &display->name))
+                        continue;
+                }
+            }
+        }
+
+        if(CGDisplayIsBuiltin(screen))
+            ffStrbufAppendS(&display->name, "built-in");
+        else
+            ffStrbufAppendF(&display->name, "external-%u", (unsigned) screen);
     }
 
     return NULL;
