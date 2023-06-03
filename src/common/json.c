d5557976d5e875a2b0eca1a7774e1fdcfeff6529
李通洲 <carter.li@eoitek.com>
2023-06-04 01:30:01 +0800

    JSON: inline json-c function wrappers

--- src/common/json.c
+++ src/common/json.c
@@ -2,27 +2,7 @@
 
 #ifdef FF_HAVE_JSONC
 
-#include "common/library.h"
 #include "common/thread.h"
-
-#include <assert.h>
-
-typedef struct FFJsonLibrary
-{
-    FF_LIBRARY_SYMBOL(json_tokener_parse)
-    FF_LIBRARY_SYMBOL(json_object_is_type)
-    FF_LIBRARY_SYMBOL(json_object_get_array)
-    FF_LIBRARY_SYMBOL(json_object_get_boolean)
-    FF_LIBRARY_SYMBOL(json_object_get_double)
-    FF_LIBRARY_SYMBOL(json_object_get_int)
-    FF_LIBRARY_SYMBOL(json_object_get_string_len)
-    FF_LIBRARY_SYMBOL(json_object_get_string)
-    FF_LIBRARY_SYMBOL(json_object_get_object)
-    FF_LIBRARY_SYMBOL(json_object_object_del)
-    FF_LIBRARY_SYMBOL(json_object_object_get)
-    FF_LIBRARY_SYMBOL(json_object_object_length)
-    FF_LIBRARY_SYMBOL(json_object_put)
-} FFJsonLibrary;
 
 static const FFJsonLibrary* loadLibSymbols(const FFinstance* instance)
 {
@@ -72,84 +52,6 @@ bool ffJsonLoadLibrary(const FFinstance* instance)
         ffThreadMutexUnlock(&mutex);
     }
     return !!ffJsonLib;
-}
-
-struct json_object *json_tokener_parse(const char *str)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_tokener_parse(str);
-}
-
-int json_object_is_type(const json_object *obj, enum json_type type)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_is_type(obj, type);
-}
-
-struct array_list *json_object_get_array(const json_object *obj)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_get_array(obj);
-}
-
-json_bool json_object_get_boolean(const json_object *obj)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_get_boolean(obj);
-}
-
-double json_object_get_double(const json_object *obj)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_get_double(obj);
-}
-
-int32_t json_object_get_int(const json_object *obj)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_get_int(obj);
-}
-
-int json_object_get_string_len(const json_object *obj)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_get_string_len(obj);
-}
-
-const char *json_object_get_string(json_object *obj)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_get_string(obj);
-}
-
-struct lh_table *json_object_get_object(const json_object *obj)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_get_object(obj);
-}
-
-void json_object_object_del(struct json_object *obj, const char *key)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_object_del(obj, key);
-}
-
-struct json_object *json_object_object_get(const json_object *obj, const char *key)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_object_get(obj, key);
-}
-
-int json_object_object_length(const json_object *obj)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_object_length(obj);
-}
-
-int json_object_put(json_object *obj)
-{
-    assert(ffJsonLib);
-    return ffJsonLib->ffjson_object_put(obj);
 }
 
 #endif
