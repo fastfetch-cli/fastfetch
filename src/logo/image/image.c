71beb626c30c5755613d743d9349f7a95effbe6c
李通洲 <carter.li@eoitek.com>
2023-06-11 21:35:18 +0800

    Logo: fix `paddingLeft` doesn't work when `paddingTop` is set

--- src/logo/image/image.c
+++ src/logo/image/image.c
@@ -47,8 +47,8 @@ static bool printImageIterm(FFinstance* instance)
         return false;
     }
 
-    ffPrintCharTimes(' ', instance->config.logo.paddingLeft);
     ffPrintCharTimes('\n', instance->config.logo.paddingTop);
+    ffPrintCharTimes(' ', instance->config.logo.paddingLeft);
 
     FF_STRBUF_AUTO_DESTROY base64 = base64Encode(&buf);
     instance->state.logoWidth = instance->config.logo.width + instance->config.logo.paddingLeft + instance->config.logo.paddingRight;
