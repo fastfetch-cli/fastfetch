0b00b5ffe0dc2fbd268462bc774a00e0cbb76e52
李通洲 <zhangsongcui3371@sina.com>
2023-06-15 09:35:47 +0800

    FreeBSD (CPU): fix temperature detection

--- src/detection/cpu/cpu_bsd.c
+++ src/detection/cpu/cpu_bsd.c
@@ -7,9 +7,8 @@ void ffDetectCPUImpl(const FFinstance* instance, FFCPUResult* cpu)
 
     if (instance->config.cpuTemp)
     {
-        FFstrbuf cpuTemp;
-        ffStrbufInit(&cpuTemp);
-        if(ffSysctlGetString("temperature", &cpuTemp))
+        FF_STRBUF_AUTO_DESTROY cpuTemp = ffStrbufCreate();
+        if(ffSysctlGetString("hw.acpi.thermal.tz0.temperature", &cpuTemp))
             cpu->temperature = FF_CPU_TEMP_UNSET;
         else
             cpu->temperature = ffStrbufToDouble(&cpuTemp);
