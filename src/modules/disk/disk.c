a5c838e4a0149800605cb946413fbc55a02d73e5
李通洲 <zhangsongcui3371@sina.com>
2023-07-16 23:00:39 +0800

    Disk: print mount points as links

--- src/modules/disk/disk.c
+++ src/modules/disk/disk.c
@@ -15,7 +15,10 @@ static void printDisk(FFDiskOptions* options, const FFDisk* disk)
 
     if(options->moduleArgs.key.length == 0)
     {
-        ffStrbufAppendF(&key, "%s (%s)", FF_DISK_MODULE_NAME, disk->mountpoint.chars);
+        if(instance.config.pipe)
+            ffStrbufAppendF(&key, "%s (%s)", FF_DISK_MODULE_NAME, disk->mountpoint.chars);
+        else
+            ffStrbufAppendF(&key, "%s (\e]8;;file://%s\e\\%s\e]8;;\e\\)", FF_DISK_MODULE_NAME, disk->mountpoint.chars, disk->mountpoint.chars);
     }
     else
     {
