0c2edd4064685b4ff6b42884dd92a6f5f96c4a1e
李通洲 <zhangsongcui3371@sina.com>
2023-08-05 00:29:04 +0800

    Memory / Swap (Windows): simplify implementation

--- src/detection/swap/swap_windows.c
+++ src/detection/swap/swap_windows.c
@@ -1,5 +1,4 @@
 #include "swap.h"
-#include "util/mallocHelper.h"
 
 #include <winternl.h>
 #include <ntstatus.h>
@@ -10,22 +9,11 @@ const char* ffDetectSwap(FFSwapResult* swap)
     SYSTEM_INFO sysInfo;
     GetNativeSystemInfo(&sysInfo);
 
-    ULONG size = sizeof(SYSTEM_PAGEFILE_INFORMATION);
-    SYSTEM_PAGEFILE_INFORMATION* FF_AUTO_FREE pstart = (SYSTEM_PAGEFILE_INFORMATION*)malloc(size);
-    while(true)
-    {
-        NTSTATUS status = NtQuerySystemInformation(SystemPagefileInformation, pstart, size, &size);
-        if(status == STATUS_INFO_LENGTH_MISMATCH)
-        {
-            if(!(pstart = (SYSTEM_PAGEFILE_INFORMATION*)realloc(pstart, size)))
-                return "realloc(pstart, size) failed";
-        }
-        else if(!NT_SUCCESS(status))
-            return "NtQuerySystemInformation(SystemPagefileInformation, size) failed";
-        break;
-    }
-    swap->bytesUsed = (uint64_t)pstart->TotalUsed * sysInfo.dwPageSize;
-    swap->bytesTotal = (uint64_t)pstart->CurrentSize * sysInfo.dwPageSize;
+    SYSTEM_PAGEFILE_INFORMATION info;
+    if (!NT_SUCCESS(NtQuerySystemInformation(SystemPagefileInformation, &info, sizeof(info), NULL)))
+        return "NtQuerySystemInformation(SystemPagefileInformation) failed";
+    swap->bytesUsed = (uint64_t)info.TotalUsed * sysInfo.dwPageSize;
+    swap->bytesTotal = (uint64_t)info.CurrentSize * sysInfo.dwPageSize;
 
     return NULL;
 }
