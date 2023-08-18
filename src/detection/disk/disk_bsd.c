c70b9cb84ebccf1c7b1b10dbc28ac176f9a842ac
李通洲 <zhangsongcui3371@sina.com>
2023-08-18 21:21:34 +0800

    Revert "Disk (BSD): try detecting disk label"
    Doesn't work
    
    This reverts commit 55b9e274b664a82f55fa4b47e00b017794d50bd5.

--- src/detection/disk/disk_bsd.c
+++ src/detection/disk/disk_bsd.c
@@ -3,8 +3,6 @@
 #include <sys/mount.h>
 
 #ifdef __FreeBSD__
-#include <sys/disklabel.h>
-
 static void detectFsInfo(struct statfs* fs, FFDisk* disk)
 {
     if(
@@ -23,9 +21,6 @@ static void detectFsInfo(struct statfs* fs, FFDisk* disk)
         disk->type = FF_DISK_TYPE_REGULAR_BIT;
 
     ffStrbufInit(&disk->name);
-    struct disklabel* lab = getdiskbyname(fs->f_mntfromname);
-    if(lab)
-        ffStrbufSetS(&disk->name, lab->d_packname);
 }
 #else
 void detectFsInfo(struct statfs* fs, FFDisk* disk);
