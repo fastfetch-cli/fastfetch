acd353b8b3c95db2672a18d67ada2e642afa074d
李通洲 <zhangsongcui3371@sina.com>
2023-08-30 14:13:26 +0800

    Chassis: add Json printing support

--- src/modules/chassis/chassis.h
+++ src/modules/chassis/chassis.h
@@ -9,3 +9,4 @@ void ffInitChassisOptions(FFChassisOptions* options);
 bool ffParseChassisCommandOptions(FFChassisOptions* options, const char* key, const char* value);
 void ffDestroyChassisOptions(FFChassisOptions* options);
 void ffParseChassisJsonObject(FFChassisOptions* options, yyjson_val* module);
+void ffGenerateChassisJson(FFChassisOptions* options, yyjson_mut_doc* doc, yyjson_mut_val* module);
