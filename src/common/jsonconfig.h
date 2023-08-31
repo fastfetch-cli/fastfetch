21f36b564f9d38fbfaa040dbbea860eb38b20cc5
李通洲 <zhangsongcui3371@sina.com>
2023-08-31 15:26:55 +0800

    Font: add JSON printing support

--- src/common/jsonconfig.h
+++ src/common/jsonconfig.h
@@ -16,6 +16,12 @@ yyjson_api_inline bool yyjson_mut_obj_add_strbuf(yyjson_mut_doc *doc,
     return yyjson_mut_obj_add_strncpy(doc, obj, _key, buf->chars, buf->length);
 }
 
+yyjson_api_inline bool yyjson_mut_arr_add_strbuf(yyjson_mut_doc *doc,
+                                                  yyjson_mut_val *obj,
+                                                  const FFstrbuf* buf) {
+    return yyjson_mut_arr_add_strncpy(doc, obj, buf->chars, buf->length);
+}
+
 yyjson_api_inline yyjson_mut_val* yyjson_mut_obj_add_obj(yyjson_mut_doc *doc,
                                                           yyjson_mut_val *obj,
                                                           const char *_key) {
--- src/modules/font/font.h
+++ src/modules/font/font.h
@@ -9,3 +9,4 @@ void ffInitFontOptions(FFFontOptions* options);
 bool ffParseFontCommandOptions(FFFontOptions* options, const char* key, const char* value);
 void ffDestroyFontOptions(FFFontOptions* options);
 void ffParseFontJsonObject(FFFontOptions* options, yyjson_val* module);
+void ffGenerateFontJson(FFFontOptions* options, yyjson_mut_doc* doc, yyjson_mut_val* module);
