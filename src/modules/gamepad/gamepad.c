97293ddee12eea3e439bb44eef67d884541385c1
李通洲 <zhangsongcui3371@sina.com>
2023-08-31 16:03:05 +0800

    Gamepad: add JSON printing support

--- src/modules/gamepad/gamepad.c
+++ src/modules/gamepad/gamepad.c
@@ -51,7 +51,7 @@ void ffPrintGamepad(FFGamepadOptions* options)
 
 void ffInitGamepadOptions(FFGamepadOptions* options)
 {
-    ffOptionInitModuleBaseInfo(&options->moduleInfo, FF_GAMEPAD_MODULE_NAME, ffParseGamepadCommandOptions, ffParseGamepadJsonObject, ffPrintGamepad, NULL);
+    ffOptionInitModuleBaseInfo(&options->moduleInfo, FF_GAMEPAD_MODULE_NAME, ffParseGamepadCommandOptions, ffParseGamepadJsonObject, ffPrintGamepad, ffGenerateGamepadJson);
     ffOptionInitModuleArg(&options->moduleArgs);
 }
 
@@ -84,5 +84,38 @@ void ffParseGamepadJsonObject(FFGamepadOptions* options, yyjson_val* module)
             continue;
 
         ffPrintError(FF_GAMEPAD_MODULE_NAME, 0, &options->moduleArgs, "Unknown JSON key %s", key);
+    }
+}
+
+void ffGenerateGamepadJson(FF_MAYBE_UNUSED FFGamepadOptions* options, yyjson_mut_doc* doc, yyjson_mut_val* module)
+{
+    FF_LIST_AUTO_DESTROY result = ffListCreate(sizeof(FFGamepadDevice));
+
+    const char* error = ffDetectGamepad(&result);
+
+    if(error)
+    {
+        yyjson_mut_obj_add_str(doc, module, "error", error);
+        return;
+    }
+
+    if(!result.length)
+    {
+        yyjson_mut_obj_add_str(doc, module, "error", "No devices detected");
+        return;
+    }
+
+    yyjson_mut_val* arr = yyjson_mut_obj_add_arr(doc, module, "result");
+    FF_LIST_FOR_EACH(FFGamepadDevice, device, result)
+    {
+        yyjson_mut_val* obj = yyjson_mut_arr_add_obj(doc, arr);
+        yyjson_mut_obj_add_strbuf(doc, obj, "identifier", &device->identifier);
+        yyjson_mut_obj_add_strbuf(doc, obj, "name", &device->name);
+    }
+
+    FF_LIST_FOR_EACH(FFGamepadDevice, device, result)
+    {
+        ffStrbufDestroy(&device->identifier);
+        ffStrbufDestroy(&device->name);
     }
 }
--- src/modules/gamepad/gamepad.h
+++ src/modules/gamepad/gamepad.h
@@ -9,3 +9,4 @@ void ffInitGamepadOptions(FFGamepadOptions* options);
 bool ffParseGamepadCommandOptions(FFGamepadOptions* options, const char* key, const char* value);
 void ffDestroyGamepadOptions(FFGamepadOptions* options);
 void ffParseGamepadJsonObject(FFGamepadOptions* options, yyjson_val* module);
+void ffGenerateGamepadJson(FFGamepadOptions* options, yyjson_mut_doc* doc, yyjson_mut_val* module);
