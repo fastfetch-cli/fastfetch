3a8819d42b5223277fc2176d8f52746692fb3b47
李通洲 <zhangsongcui3371@sina.com>
2023-09-13 14:01:15 +0800

    Wifi: support JSON format

--- src/modules/wifi/wifi.h
+++ src/modules/wifi/wifi.h
@@ -9,3 +9,4 @@ void ffInitWifiOptions(FFWifiOptions* options);
 bool ffParseWifiCommandOptions(FFWifiOptions* options, const char* key, const char* value);
 void ffDestroyWifiOptions(FFWifiOptions* options);
 void ffParseWifiJsonObject(FFWifiOptions* options, yyjson_val* module);
+void ffGenerateWifiJson(FFWifiOptions* options, yyjson_mut_doc* doc, yyjson_mut_val* module);
