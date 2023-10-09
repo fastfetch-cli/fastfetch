2d2e0ddc6d83eee97ca7dec38cbf24ddbc724e44
李通洲 <zhangsongcui3371@sina.com>
2023-10-09 13:28:30 +0800

    Libc (FreeBSD): detect FBSD version

--- src/detection/libc/libc_nosupport.c
+++
@@ -1,9 +0,0 @@
-#include "libc.h"
-
-#define FF_STR_INDIR(x) #x
-#define FF_STR(x) FF_STR_INDIR(x)
-
-const char* ffDetectLibc(FF_MAYBE_UNUSED FFLibcResult* result)
-{
-    return "Not supported on this platform";
-}
