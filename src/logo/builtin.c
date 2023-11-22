eea6e81276bfbb0bbc2c389f3e8aeff215a81ee3
李通洲 <zhangsongcui3371@sina.com>
2023-11-22 09:06:54 +0800

    Logo (Builtin): add XrayOS
    Ref: https://github.com/dylanaraps/neofetch/pull/2413

--- src/logo/builtin.c
+++ src/logo/builtin.c
@@ -4240,6 +4240,16 @@ static const FFlogo X[] = {
             FF_COLOR_FG_CYAN,
         },
     },
+    //XrayOS
+    {
+        .names = {"XrayOS"},
+        .lines = FASTFETCH_DATATEXT_LOGO_XRAYOS,
+        .colors = {
+            FF_COLOR_FG_256 "15",
+            FF_COLOR_FG_256 "14",
+            FF_COLOR_FG_256 "16",
+        }
+    },
     // LAST
     {},
 };
