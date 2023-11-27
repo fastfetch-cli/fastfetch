e67a6856cee50aa1f971a1198031c88091efad3a
李通洲 <zhangsongcui3371@sina.com>
2023-11-27 15:21:48 +0800

    Completion (Fish): use `fastfetch --help-raw`

--- completions/fish.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import json, subprocess
-
-if __name__ == "__main__":
-    data: dict[str, str] = json.loads(subprocess.check_output(['fastfetch', '--help-raw']))
-    for item in data:
-        print(item)
