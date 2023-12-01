36298b8e93f735542c8a2e85b25a80395550eba1
李通洲 <zhangsongcui3371@sina.com>
2023-12-01 23:23:29 +0800

    Swap (FreeBSD): fix impl

--- src/detection/swap/swap_bsd.c
+++ src/detection/swap/swap_bsd.c
@@ -1,9 +1,32 @@
 #include "swap.h"
 #include "common/sysctl.h"
 
+#include <vm/vm_param.h>
+
 const char* ffDetectSwap(FFSwapResult* swap)
 {
-    swap->bytesTotal = (uint64_t)ffSysctlGetInt64("vm.swap_total", 0);
-    swap->bytesUsed = (uint64_t)ffSysctlGetInt64("vm.swap_reserved", 0);
+    int mib[16];
+    size_t mibsize = sizeof(mib) / sizeof(*mib);
+    if (sysctlnametomib("vm.swap_info", mib, &mibsize) < 0)
+        return "sysctlnametomib(\"vm.swap_info\") failed";
+
+    swap->bytesUsed = swap->bytesTotal = 0;
+
+    for (int n = 0; ; ++n)
+    {
+        mib[mibsize] = n;
+        struct xswdev xsw;
+        size_t size = sizeof(xsw);
+        if (sysctl(mib, (uint32_t) (mibsize + 1), &xsw, &size, NULL, 0) < 0)
+            break;
+        if (xsw.xsw_version != XSWDEV_VERSION)
+            return "xswdev version mismatch";
+        swap->bytesUsed += (uint64_t) xsw.xsw_used;
+        swap->bytesTotal += (uint64_t) xsw.xsw_nblks;
+    }
+
+    swap->bytesUsed *= instance.state.platform.pageSize;
+    swap->bytesTotal *= instance.state.platform.pageSize;
+
     return NULL;
 }
