a9c5c2892c0cda2b36c5c1a88d5c766e00c91e75
李通洲 <zhangsongcui3371@sina.com>
2023-12-07 16:30:10 +0800

    Memory (Linux): Use `MemAvailable` if available

--- src/detection/memory/memory_linux.c
+++ src/detection/memory/memory_linux.c
@@ -13,33 +13,43 @@ const char* ffDetectMemory(FFMemoryResult* ram)
     buf[nRead] = '\0';
 
     uint64_t memTotal = 0,
+             memAvailable = 0,
              shmem = 0,
              memFree = 0,
              buffers = 0,
              cached = 0,
              sReclaimable = 0;
-    
+
     char *token = NULL;
     if((token = strstr(buf, "MemTotal:")) != NULL)
         memTotal = strtoul(token + strlen("MemTotal:"), NULL, 10);
+    else
+        return "MemTotal not found in /proc/meminfo";
 
-    if((token = strstr(buf, "MemFree:")) != NULL)
-        memFree = strtoul(token + strlen("MemFree:"), NULL, 10);
+    if((token = strstr(buf, "MemAvailable:")) != NULL)
+        memAvailable = strtoul(token + strlen("MemAvailable:"), NULL, 10);
+    else
+    {
+        if((token = strstr(buf, "MemFree:")) != NULL)
+            memFree = strtoul(token + strlen("MemFree:"), NULL, 10);
 
-    if((token = strstr(buf, "Buffers:")) != NULL)
-        buffers = strtoul(token + strlen("Buffers:"), NULL, 10);
-    
-    if((token = strstr(buf, "Cached:")) != NULL)
-        cached = strtoul(token + strlen("Cached:"), NULL, 10);
-    
-    if((token = strstr(buf, "Shmem:")) != NULL)
-        shmem = strtoul(token + strlen("Shmem:"), NULL, 10);
-    
-    if((token = strstr(buf, "SReclaimable:")) != NULL)
-        sReclaimable = strtoul(token + strlen("SReclaimable:"), NULL, 10);
+        if((token = strstr(buf, "Buffers:")) != NULL)
+            buffers = strtoul(token + strlen("Buffers:"), NULL, 10);
+
+        if((token = strstr(buf, "Cached:")) != NULL)
+            cached = strtoul(token + strlen("Cached:"), NULL, 10);
+
+        if((token = strstr(buf, "Shmem:")) != NULL)
+            shmem = strtoul(token + strlen("Shmem:"), NULL, 10);
+
+        if((token = strstr(buf, "SReclaimable:")) != NULL)
+            sReclaimable = strtoul(token + strlen("SReclaimable:"), NULL, 10);
+
+        memAvailable = memFree + buffers + cached + sReclaimable - shmem;
+    }
 
     ram->bytesTotal = memTotal * 1024lu;
-    ram->bytesUsed = (memTotal + shmem - memFree - buffers - cached - sReclaimable) * 1024lu;
+    ram->bytesUsed = (memTotal - memAvailable) * 1024lu;
 
     return NULL;
 }
