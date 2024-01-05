2340c2afd726877f93d44f89d431940c90a9ef67
李通洲 <zhangsongcui3371@sina.com>
2024-01-05 16:15:53 +0800

    Board (macOS): print internal name instead of model indentifier
    and actually detect the vendor

--- src/detection/board/board_apple.c
+++ src/detection/board/board_apple.c
@@ -1,13 +1,21 @@
 #include "board.h"
 
 #include "common/sysctl.h"
+#include "util/apple/cf_helpers.h"
 
 const char* ffDetectBoard(FFBoardResult* result)
 {
-    const char* error = ffSysctlGetString("hw.model", &result->name);
-    if (error)
-        return error;
+    FF_IOOBJECT_AUTO_RELEASE io_registry_entry_t service = IOServiceGetMatchingService(MACH_PORT_NULL, IOServiceMatching("IOPlatformExpertDevice"));
+    if (!service)
+        return "No IOPlatformExpertDevice found";
 
-    ffStrbufSetStatic(&result->vendor, "Apple");
+    io_name_t name;
+    if (IORegistryEntryGetName(service, name) == kIOReturnSuccess)
+        ffStrbufSetS(&result->name, name);
+
+    FF_CFTYPE_AUTO_RELEASE CFTypeRef manufacturer = IORegistryEntryCreateCFProperty(service, CFSTR("manufacturer"), kCFAllocatorDefault, kNilOptions);
+    if (manufacturer)
+        ffCfStrGetString(manufacturer, &result->vendor);
+
     return NULL;
 }
