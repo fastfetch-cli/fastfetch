691cb4ac2455b1441131f9e684754cdcb9a82aa0
李通洲 <zhangsongcui3371@sina.com>
2024-04-09 20:37:43 +0800

    Global: use time string when generating JSON result

--- src/common/time.h
+++ src/common/time.h
@@ -1,12 +1,12 @@
 #pragma once
 
 #include <stdint.h>
+#include <time.h>
+#include <stdio.h>
 #ifdef _WIN32
     #include <synchapi.h>
     #include <profileapi.h>
     #include <sysinfoapi.h>
-#else
-    #include <time.h>
 #endif
 
 static inline uint64_t ffTimeGetTick() //In msec
@@ -45,3 +45,37 @@ static inline void ffTimeSleep(uint32_t msec)
         nanosleep(&(struct timespec){ msec / 1000, (long) (msec % 1000) * 1000000 }, NULL);
     #endif
 }
+
+#ifdef _WIN32
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wformat"
+#endif
+
+// Not thread-safe
+static inline const char* ffTimeToFullStr(uint64_t msec)
+{
+    if (msec == 0) return "";
+    time_t tsec = (time_t) (msec / 1000);
+    const struct tm* tm = localtime(&tsec);
+
+    static char buf[32];
+    strftime(buf, __builtin_strlen("0000-00-00T00:00:00") + 1, "%FT%T", tm);
+    snprintf(buf + __builtin_strlen("0000-00-00T00:00:00"), __builtin_strlen(".000") + 1, ".%03u", (unsigned) (msec % 1000));
+    strftime(buf + __builtin_strlen("0000-00-00T00:00:00.000"), __builtin_strlen("+0000") + 1, "%z", tm);
+    return buf;
+}
+
+// Not thread-safe
+static inline const char* ffTimeToShortStr(uint64_t msec)
+{
+    if (msec == 0) return "";
+    time_t tsec = (time_t) (msec / 1000);
+
+    static char buf[32];
+    strftime(buf, sizeof(buf), "%F %T", localtime(&tsec));
+    return buf;
+}
+
+#ifdef _WIN32
+    #pragma GCC diagnostic pop
+#endif
