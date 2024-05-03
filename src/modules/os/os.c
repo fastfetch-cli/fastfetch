08c6a6e4cb2e854ebfd3b0b3f86630404b15bb47
李通洲 <zhangsongcui3371@sina.com>
2024-05-03 13:20:23 +0800

    OS (macOS): don't print build id
    Remove special handling

--- src/modules/os/os.c
+++ src/modules/os/os.c
@@ -40,14 +40,6 @@ static void buildOutputDefault(const FFOSResult* os, FFstrbuf* result)
         ffStrbufAppend(result, &os->version);
     }
 
-    #ifdef __APPLE__
-    if(os->buildID.length > 0)
-    {
-        ffStrbufAppendC(result, ' ');
-        ffStrbufAppend(result, &os->buildID);
-    }
-    #endif
-
     //Append variant if it is missing
     if(os->variant.length > 0 && ffStrbufFirstIndex(result, &os->variant) == result->length)
     {
