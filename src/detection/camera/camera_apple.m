d16be1308a33314e5bdb1fcd8947d61098e915d8
Carter Li <CarterLi@users.noreply.github.com>
2024-05-19 08:46:54 +0800

    Update camera_apple.m

--- src/detection/camera/camera_apple.m
+++ src/detection/camera/camera_apple.m
@@ -48,6 +48,7 @@ const char* ffDetectCamera(FFlist* result)
         camera->width = size.width < 0 ? 0 : (uint32_t) size.width;
         camera->height = size.height < 0 ? 0 : (uint32_t) size.height;
     }
+    #else
+    return "No support for old MacOS version";
     #endif
-    return NULL;
 }
