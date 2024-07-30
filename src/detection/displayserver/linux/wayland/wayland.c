ced67e81a5d39f964220e551a1f1a91b05e3d0b5
Carter Li <zhangsongcui@live.cn>
2024-07-30 23:50:19 +0800

    DisplayServer (Linux): ensure $XDG_RUNTIME_DIR is set before connecting wayland

--- src/detection/displayserver/linux/wayland/wayland.c
+++ src/detection/displayserver/linux/wayland/wayland.c
@@ -136,6 +136,9 @@ uint32_t ffWaylandHandleRotation(WaylandDisplay* display)
 
 const char* ffdsConnectWayland(FFDisplayServerResult* result)
 {
+    if (getenv("XDG_RUNTIME_DIR") == NULL)
+        return "Wayland requires $XDG_RUNTIME_DIR being set";
+
     FF_LIBRARY_LOAD(wayland, &instance.config.library.libWayland, false, "libwayland-client" FF_LIBRARY_EXTENSION, 1)
 
     FF_LIBRARY_LOAD_SYMBOL_MESSAGE(wayland, wl_display_connect)
