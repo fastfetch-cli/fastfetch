45659854d2202e56746d38048e0756bb69356d7f
Carter Li <zhangsongcui3371@sina.com>
2024-08-02 15:24:29 +0800

    GPU (Linux): don't ignore disabled GPU
    Should fix #1140

--- src/detection/gpu/gpu_linux.c
+++ src/detection/gpu/gpu_linux.c
@@ -284,14 +284,6 @@ static const char* detectPci(const FFGPUOptions* options, FFlist* gpus, FFstrbuf
     if (sscanf(pPciPath, "%" SCNx32 ":%" SCNx32 ":%" SCNx32 ".%" SCNx32, &pciDomain, &pciBus, &pciDevice, &pciFunc) != 4)
         return "Invalid PCI device path";
 
-    ffStrbufAppendS(deviceDir, "/enable");
-    if (ffReadFileBuffer(deviceDir->chars, buffer))
-    {
-        if (!ffStrbufStartsWithC(buffer, '1'))
-            return "GPU disabled";
-    }
-    ffStrbufSubstrBefore(deviceDir, drmDirPathLength);
-
     FFGPUResult* gpu = (FFGPUResult*)ffListAdd(gpus);
     ffStrbufInitStatic(&gpu->vendor, ffGetGPUVendorString((uint16_t) vendorId));
     ffStrbufInit(&gpu->name);
