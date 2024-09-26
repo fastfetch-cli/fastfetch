28c9b674149bd8f0d5106a73fdbce57a6b1f2558
李通洲 <carter.li@eoitek.com>
2024-09-26 14:41:06 +0800

    Processing (Windows): correctly escaping arguments

--- src/common/processing_windows.c
+++ src/common/processing_windows.c
@@ -8,6 +8,43 @@
 
 enum { FF_PIPE_BUFSIZ = 8192 };
 
+static void argvToCmdline(char* const argv[], FFstrbuf* result)
+{
+    // From https://gist.github.com/jin-x/cdd641d98887524b091fb1f82a68717d
+
+    FF_STRBUF_AUTO_DESTROY temp = ffStrbufCreate();
+    for (int i = 0; argv[i] != NULL; i++)
+    {
+        ffStrbufSetS(&temp, argv[i]);
+        // Add slash (\) before double quotes (") and duplicate slashes before it
+        for (
+            uint32_t pos = ffStrbufFirstIndexC(&temp, '"'), cnt;
+            pos != temp.length;
+            pos = ffStrbufNextIndexC(&temp, pos + cnt * 2, '"')
+        ) {
+            cnt = 1;
+            while (pos > 0 && temp.chars[pos - 1] == '\\') { ++cnt, --pos; }
+            ffStrbufInsertNC(&temp, pos, cnt, '\\');
+        }
+
+        // Add quotes around string if whitespace chars are present (with slash duplicating at the end of string)
+        if (ffStrbufFirstIndexS(&temp, " \t") != temp.length)
+        {
+            uint32_t pos = temp.length;
+            uint32_t cnt = 0;
+            while (pos > 0 && temp.chars[pos - 1] == '\\') { ++cnt, --pos; }
+            if (cnt > 0) ffStrbufAppendNC(&temp, cnt, '\\');
+            ffStrbufPrependC(&temp, '"');
+            ffStrbufAppendC(&temp, '"');
+        }
+
+        // Add space delimiter
+        if (i > 0) ffStrbufAppendC(result, ' ');
+        ffStrbufAppend(result, &temp);
+        ffStrbufClear(&temp);
+    }
+}
+
 const char* ffProcessAppendOutput(FFstrbuf* buffer, char* const argv[], bool useStdErr)
 {
     int timeout = instance.config.general.processingTimeout;
@@ -55,12 +92,8 @@ const char* ffProcessAppendOutput(FFstrbuf* buffer, char* const argv[], bool use
         else
             siStartInfo.hStdOutput = hChildPipeWrite;
 
-        FF_STRBUF_AUTO_DESTROY cmdline = ffStrbufCreateF("\"%s\"", argv[0]);
-        for(char* const* parg = &argv[1]; *parg; ++parg)
-        {
-            ffStrbufAppendC(&cmdline, ' ');
-            ffStrbufAppendS(&cmdline, *parg);
-        }
+        FF_STRBUF_AUTO_DESTROY cmdline = ffStrbufCreate();
+        argvToCmdline(argv, &cmdline);
 
         success = CreateProcessA(
             NULL,          // application name
