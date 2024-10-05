05075a2d6596b80439ce296998c075acf11b8acf
xeroarch <wget_pizza@proton.me>
2024-10-04 23:14:12 -0400

    upgrading fetch ascii logo

--- src/logo/builtin.c
+++ src/logo/builtin.c
@@ -4791,9 +4791,11 @@ static const FFlogo X[] = {
         .lines = FASTFETCH_DATATEXT_LOGO_XEROARCH,
         .colors = {
             FF_COLOR_FG_256 "16",
-            FF_COLOR_FG_256 "51",
+            FF_COLOR_FG_256 "14",
+            FF_COLOR_FG_256 "50",
+            FF_COLOR_FG_256 "93",
+            FF_COLOR_FG_256 "46",
             FF_COLOR_FG_256 "15",
-            FF_COLOR_FG_256 "51",
         }
     },
     //Xray_OS
