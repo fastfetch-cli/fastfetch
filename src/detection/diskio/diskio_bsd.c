5c21931a097def94a1424938723434791c13c2c2
Carter Li <zhangsongcui3371@sina.com>
2024-11-03 18:05:56 +0800

    DiskIO (DragonFly): add support

--- src/detection/diskio/diskio_bsd.c
+++ src/detection/diskio/diskio_bsd.c
@@ -61,9 +61,49 @@ const char* ffDiskIOGetIoCounters(FFlist* result, FFDiskIOOptions* options)
 
     return NULL;
 }
+
 #else
+
+#include <devstat.h>
+#include <memory.h>
+
 const char* ffDiskIOGetIoCounters(FFlist* result, FFDiskIOOptions* options)
 {
-    return "Fastfetch was compiled without libgeom support";
+    if (checkversion() < 0)
+        return "checkversion() failed";
+
+    struct statinfo stats = {
+        .dinfo = (struct devinfo *)calloc(1, sizeof(struct devinfo)),
+    };
+    if (getdevs(&stats) < 0)
+        return "getdevs() failed";
+
+    for (int i = 0; i < stats.dinfo->numdevs; i++)
+    {
+        struct devstat* current = &stats.dinfo->devices[i];
+        if (current->device_type & DEVSTAT_TYPE_PASS)
+            continue;
+
+        char deviceName[128];
+        snprintf(deviceName, sizeof(deviceName), "%s%d", current->device_name, current->unit_number);
+
+        if (options->namePrefix.length && strncmp(deviceName, options->namePrefix.chars, options->namePrefix.length) != 0)
+            continue;
+
+        FFDiskIOResult* device = (FFDiskIOResult*) ffListAdd(result);
+        ffStrbufInitS(&device->name, deviceName);
+        ffStrbufInitF(&device->devPath, "/dev/%s", deviceName);
+        device->bytesRead = current->bytes_read;
+        device->readCount = current->num_reads;
+        device->bytesWritten = current->bytes_written;
+        device->writeCount = current->num_writes;
+    }
+
+    if (stats.dinfo->mem_ptr)
+        free(stats.dinfo->mem_ptr);
+    free(stats.dinfo);
+
+    return NULL;
 }
+
 #endif
