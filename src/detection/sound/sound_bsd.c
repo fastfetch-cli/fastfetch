1c4953a467bb9ba84011eb5f57f66281b52a80d5
李通洲 <zhangsongcui3371@sina.com>
2025-01-26 21:03:27 +0800

    Sound (NetBSD): use OSS instead of pulseaudio
    Fix #1527

--- src/detection/sound/sound_bsd.c
+++ src/detection/sound/sound_bsd.c
@@ -4,14 +4,27 @@
 
 #include <fcntl.h>
 #include <sys/soundcard.h>
+#include <unistd.h>
 
 const char* ffDetectSound(FFlist* devices)
 {
-    char path[] = "/dev/mixer0";
+    #ifndef __NetBSD__
     int defaultDev = ffSysctlGetInt("hw.snd.default_unit", -1);
-
     if (defaultDev == -1)
         return "sysctl(hw.snd.default_unit) failed";
+    #else
+    int defaultDev;
+    {
+        char mixerp[8];
+        if (readlink("/dev/mixer", mixerp, ARRAY_SIZE(mixerp)) != 6)
+            return "readlink(/dev/mixer) failed";
+        defaultDev = mixerp[5] - '0';
+        if (defaultDev < 0 || defaultDev > 9)
+            return "Invalid mixer device";
+    }
+    #endif
+
+    char path[] = "/dev/mixer0";
 
     struct oss_sysinfo info = { .nummixers = 9 };
 
@@ -33,8 +46,11 @@ const char* ffDetectSound(FFlist* devices)
         if (!(devmask & SOUND_MASK_VOLUME))
             continue;
 
+        #if defined(SOUND_MIXER_MUTE) && (SOUND_MIXER_MUTE != SOUND_MIXER_NONE)
+        #define FF_SOUND_HAVE_MIXER_MUTE 1
         uint32_t mutemask = 0;
-        ioctl(fd, SOUND_MIXER_READ_MUTE, &mutemask); // doesn't seem to be available on DragonFly
+        ioctl(fd, SOUND_MIXER_READ_MUTE, &mutemask);
+        #endif
 
         struct oss_card_info ci = { .card = idev };
         if (ioctl(fd, SNDCTL_CARDINFO, &ci) < 0)
@@ -49,9 +65,11 @@ const char* ffDetectSound(FFlist* devices)
         ffStrbufInitF(&device->name, "%s %s", ci.longname, ci.hw_info);
         ffStrbufTrimRightSpace(&device->name);
         ffStrbufInitF(&device->platformApi, "%s %s", info.product, info.version);
-        device->volume = mutemask & SOUND_MASK_VOLUME
-            ? 0
-            : ((uint8_t) volume /*left*/ + (uint8_t) (volume >> 8) /*right*/) / 2;
+        device->volume =
+        #ifdef FF_SOUND_HAVE_MIXER_MUTE
+            mutemask & SOUND_MASK_VOLUME ? 0 :
+        #endif
+            ((uint8_t) volume /*left*/ + (uint8_t) (volume >> 8) /*right*/) / 2;
         device->active = true;
         device->main = defaultDev == idev;
     }
