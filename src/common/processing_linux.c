7f212025dea41df2369ed922b46bf27932f653bf
李通洲 <zhangsongcui3371@sina.com>
2025-02-05 15:36:09 +0800

    Processing (Unix): better `pipe2(2)` existance check

--- src/common/processing_linux.c
+++ src/common/processing_linux.c
@@ -32,9 +32,9 @@
 
 enum { FF_PIPE_BUFSIZ = 8192 };
 
-static inline int ffPipe2(int *fds, int flags)
+static inline int ffPipe2(int* fds, int flags)
 {
-    #ifdef __APPLE__
+    #ifndef FF_HAVE_PIPE2
         if(pipe(fds) == -1)
             return -1;
         fcntl(fds[0], F_SETFL, fcntl(fds[0], F_GETFL) | flags);
