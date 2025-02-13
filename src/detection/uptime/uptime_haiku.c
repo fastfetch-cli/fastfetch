e38f094c1683058da32c0afbd82c501be9249951
Carter Li <zhangsongcui@live.cn>
2025-02-13 22:56:25 +0000

    Uptime (Haiku): silence compiler warnings

--- src/detection/uptime/uptime_haiku.c
+++ src/detection/uptime/uptime_haiku.c
@@ -1,11 +1,9 @@
 #include "uptime.h"
 #include "common/time.h"
 
-#include <OS.h>
-
 const char* ffDetectUptime(FFUptimeResult* result)
 {
-    result->uptime = system_time() / 1000;
-    result->bootTime = (real_time_clock_usecs() / 1000) - result->uptime;
+    result->uptime = (uint64_t) system_time() / 1000;
+    result->bootTime = ffTimeGetNow() - result->uptime;
     return NULL;
 }
