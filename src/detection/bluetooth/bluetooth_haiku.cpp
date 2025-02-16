41d930b1e1c116b6a952654f744ee69b6ab0740a
Carter Li <zhangsongcui@live.cn>
2025-02-16 20:37:21 +0800

    Bluetooth (Haiku): WIP

---
+++ src/detection/bluetooth/bluetooth_haiku.cpp
@@ -0,0 +1,26 @@
+extern "C" {
+#include "bluetooth.h"
+}
+
+#include <bluetooth/LocalDevice.h>
+
+const char* ffDetectBluetooth(FF_MAYBE_UNUSED FFBluetoothOptions* options, FFlist* devices /* FFBluetoothResult */)
+{
+    using namespace Bluetooth;
+    LocalDevice* dev = LocalDevice::GetLocalDevice();
+    if (!dev) return NULL;
+
+    BString devClass;
+    dev->GetDeviceClass().DumpDeviceClass(devClass);
+
+    FFBluetoothResult* device = (FFBluetoothResult*) ffListAdd(devices);
+    ffStrbufInitS(&device->name, dev->GetFriendlyName());
+    ffStrbufInitS(&device->address, bdaddrUtils::ToString(dev->GetBluetoothAddress()).String());
+    ffStrbufInitS(&device->type, devClass.String());
+    device->battery = 0;
+    device->connected = true;
+
+    // TODO: more devices?
+
+    return NULL;
+}
