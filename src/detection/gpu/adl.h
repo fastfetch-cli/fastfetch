0ae5fceb07b5dab2747998a7fcba1e1065e8c263
李通洲 <zhangsongcui3371@sina.com>
2025-03-23 23:36:01 +0800

    GPU (Windows): add display-library for future use

---
+++ src/detection/gpu/adl.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "3rdparty/display-library/adl_sdk.h"
+
+// https://gpuopen-librariesandsdks.github.io/adl/modules.html
+
+// ADLOD6CurrentStatus ADL2_Overdrive6_Temperature_Get
+// https://gpuopen-librariesandsdks.github.io/adl/group__OVERDRIVE6API.html#ga10021644bfab0e3910e10caaa7c0053c
+
+// ADL2_Adapter_DedicatedVRAMUsage_Get ADL2_Adapter_VRAMUsage_Get
+// https://gpuopen-librariesandsdks.github.io/adl/group__ADAPTERMAINAPI.html#ga9c41c3740ddd76ee4f8a33a3ccc55e7f
+
+// ADL_ASIC_DISCRETE ADL_Adapter_ASICFamilyType_Get
+// https://gpuopen-librariesandsdks.github.io/adl/group__ADAPTERMAINAPI.html#ga345082fdc7c14fc5179605732085d272
