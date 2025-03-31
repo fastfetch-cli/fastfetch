031aa985f17f3d7f796669c69fe89d671f214a99
李通洲 <zhangsongcui3371@sina.com>
2025-03-31 23:40:01 +0800

    TPM (macOS): add support

---
+++ src/detection/tpm/tpm_apple.c
@@ -0,0 +1,31 @@
+#include "tpm.h"
+
+#ifndef __aarch64__
+    #include "util/apple/cf_helpers.h"
+    #include <IOKit/IOKitLib.h>
+#endif
+
+const char* ffDetectTPM(FFTPMResult* result)
+{
+    #ifdef __aarch64__
+
+    ffStrbufSetStatic(&result->version, "2.0");
+    ffStrbufSetStatic(&result->description, "Apple Silicon Security");
+    return NULL;
+
+    #else
+
+    FF_IOOBJECT_AUTO_RELEASE io_service_t t2Service = IOServiceGetMatchingService(
+        kIOMainPortDefault,
+        IOServiceMatching("AppleT2"));
+
+    if (t2Service) {
+        ffStrbufSetStatic(&result->version, "2.0");
+        ffStrbufSetStatic(&result->description, "Apple T2 Security Chip");
+        return NULL;
+    }
+
+    #endif
+
+    return "No Apple Security hardware detected";
+}
