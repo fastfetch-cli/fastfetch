b812f3ccde4f231385cbecf6046e77fa281813b4
李通洲 <carter.li@eoitek.com>
2025-04-15 16:41:19 +0800

    Uptime (Windows): use `QueryUnbiasedInterruptTime`
    As suggested by MSDN

--- src/detection/uptime/uptime_windows.c
+++ src/detection/uptime/uptime_windows.c
@@ -1,11 +1,13 @@
 #include "uptime.h"
 #include "common/time.h"
 
-#include <sysinfoapi.h>
+#include <realtimeapiset.h>
 
 const char* ffDetectUptime(FFUptimeResult* result)
 {
-    result->uptime = GetTickCount64();
+    // According to MSDN, this function only fails if it's called with NULL
+    QueryUnbiasedInterruptTime(&result->uptime);
+    result->uptime /= 10000; // Convert from 100-nanosecond intervals to milliseconds
     result->bootTime = ffTimeGetNow() - result->uptime;
     return NULL;
 }
