eb7975b211261304b9112a637742556bcb2f1fa6
0xGeN02 <manuelmateodgl02@gmail.com>
2025-05-02 19:27:39 +0200

    fix: gen-pciids.py: resolve redefiining built-in id in vendors and devices

--- scripts/gen-pciids.py
+++ scripts/gen-pciids.py
@@ -3,19 +3,19 @@
 import sys
 
 class PciDeviceModel:
-    def __init__(self, id: int, name: str):
-        self.id = id
+    def __init__(self, device_id: int, name: str):
+        self.device_id = device_id
         self.name = name
 
 class PciVendorModel:
-    def __init__(self, id: int, name: str):
-        self.id = id
+    def __init__(self, vendor_id: int, name: str):
+        self.vendor_id = vendor_id
         self.name = name
         self.devices = []
 
 def main(keep_vendor_list: set, pci_ids_path: str):
     vendors = []
-    with open(pci_ids_path, 'r') as f:
+    with open(pci_ids_path, 'r', encoding="utf-8") as f:
         full_text = f.read()
 
     dev_list_text = full_text[:full_text.rfind('\n\n\n')]  # remove known classes
@@ -23,11 +23,11 @@ def main(keep_vendor_list: set, pci_ids_path: str):
         if not line or line[0] == '#':
             continue
         if line[0] != '\t':
-            id, name = line.split('  ', maxsplit=1)
-            vendors.append(PciVendorModel(int(id, 16), name))
+            vendor_id, name = line.split('  ', maxsplit=1)
+            vendors.append(PciVendorModel(int(vendor_id, 16), name))
         elif line[1] != '\t':
-            id, name = line[1:].split('  ', maxsplit=1)
-            vendors[-1].devices.append(PciDeviceModel(int(id, 16), name))
+            device_id, name = line[1:].split('  ', maxsplit=1)
+            vendors[-1].devices.append(PciDeviceModel(int(device_id, 16), name))
 
     code = """\
 // SPDX-License-Identifier: BSD-3-Clause
@@ -53,20 +53,20 @@ typedef struct FFPciVendor
 """
 
     if keep_vendor_list:
-        vendors = [vendor for vendor in vendors if vendor.id in keep_vendor_list]
+        vendors = [vendor for vendor in vendors if vendor.vendor_id in keep_vendor_list]
 
     for vendor in vendors:
         if vendor.devices:
-            piece = ',\n    '.join('{{ 0x{:04X}, "{}" }}'.format(device.id, device.name.replace('"', '\\"')) for device in vendor.devices)
+            piece = ',\n    '.join('{{ 0x{:04X}, "{}" }}'.format(device.device_id, device.name.replace('"', '\\"')) for device in vendor.devices)
             code += f"""
 // {vendor.name}
-static const FFPciDevice pciDevices_{vendor.id:04X}[] = {{
+static const FFPciDevice pciDevices_{vendor.vendor_id:04X}[] = {{
     {piece},
     {{}},
 }};
 """
 
-    piece = ',\n    '.join('{{ 0x{:04X}, "{}", {}, {} }}'.format(vendor.id, vendor.name.replace('"', '\\"'), vendor.devices and f"pciDevices_{vendor.id:04X}" or "NULL", len(vendor.devices)) for vendor in vendors)
+    piece = ',\n    '.join('{{ 0x{:04X}, "{}", {}, {} }}'.format(vendor.vendor_id, vendor.name.replace('"', '\\"'), vendor.devices and f"pciDevices_{vendor.vendor_id:04X}" or "NULL", len(vendor.devices)) for vendor in vendors)
     code += f"""
 const FFPciVendor ffPciVendors[] = {{
     {piece},
