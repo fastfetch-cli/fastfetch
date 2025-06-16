3fcbd80ef06d656dce75b63642bb9fb2c891bac8
李通洲 <zhangsongcui3371@sina.com>
2025-06-17 00:02:47 +0800

    Board (macOS): detect board-id if available
    Ref: https://github.com/GuillaumeGomez/sysinfo/pull/1545

--- src/detection/board/board_apple.c
+++ src/detection/board/board_apple.c
@@ -9,9 +9,19 @@ const char* ffDetectBoard(FFBoardResult* result)
     if (!service)
         return "No IOPlatformExpertDevice found";
 
-    io_name_t name;
-    if (IORegistryEntryGetName(service, name) == kIOReturnSuccess)
-        ffStrbufSetS(&result->name, name);
+    FF_CFTYPE_AUTO_RELEASE CFTypeRef boardId = IORegistryEntryCreateCFProperty(service, CFSTR("board-id"), kCFAllocatorDefault, kNilOptions);
+    if (boardId)
+        ffCfStrGetString(boardId, &result->name);
+    else
+    {
+        io_name_t name;
+        if (IORegistryEntryGetName(service, name) == kIOReturnSuccess)
+            ffStrbufSetS(&result->name, name);
+    }
+
+    FF_CFTYPE_AUTO_RELEASE CFStringRef version = IORegistryEntryCreateCFProperty(service, CFSTR("version"), kCFAllocatorDefault, kNilOptions);
+    if (version)
+        ffCfStrGetString(version, &result->version);
 
     FF_CFTYPE_AUTO_RELEASE CFTypeRef manufacturer = IORegistryEntryCreateCFProperty(service, CFSTR("manufacturer"), kCFAllocatorDefault, kNilOptions);
     if (manufacturer)
