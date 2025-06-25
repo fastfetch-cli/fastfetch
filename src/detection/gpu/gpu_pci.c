863c5583ef43380ad7ecb392953f1a2ada6fca2f
李通洲 <zhangsongcui3371@sina.com>
2025-06-25 13:42:46 +0800

    GPU (OpenBSD): fix pci.ids file location
    Fix #1819
    
    Signed-off-by: kevlo <kevlo@openbsd.org>

--- src/detection/gpu/gpu_pci.c
+++ src/detection/gpu/gpu_pci.c
@@ -45,8 +45,10 @@ static const FFstrbuf* loadPciIds()
             if (pciids.length == 0)
                 ffReadFileBuffer(FASTFETCH_TARGET_DIR_USR "/local/share/hwdata/pci.ids", &pciids);
         }
-        #elif __FreeBSD__ || __OpenBSD__ || __NetBSD__
+        #elif __FreeBSD__ || __NetBSD__
         ffReadFileBuffer(_PATH_LOCALBASE "/share/pciids/pci.ids", &pciids);
+        #elif __OpenBSD__
+        ffReadFileBuffer(_PATH_LOCALBASE "/share/hwdata/pci.ids", &pciids);
         #elif __sun
         ffReadFileBuffer(FASTFETCH_TARGET_DIR_ROOT "/usr/share/hwdata/pci.ids", &pciids);
         #elif __HAIKU__
