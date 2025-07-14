285b898011bdeab2f3fb0dae3728c642d37316dd
Cybersecbyte <cybersecbyte@gmail.com>
2025-07-14 21:32:19 +0530

    Logo (Builtin): Add Zraxyl

--- src/logo/builtin.c
+++ src/logo/builtin.c
@@ -5443,6 +5443,16 @@ static const FFlogo Z[] = {
             FF_COLOR_FG_BLUE,
         },
     },
+    // Zraxyl
+    {
+        .names = {"Zraxyl" },
+        .lines = FASTFETCH_DATATEXT_LOGO_ZRAXYL,
+        .colors = {
+            FF_COLOR_FG_BLUE,
+        },
+        .colorKeys = FF_COLOR_FG_BLUE,
+        .colorTitle = FF_COLOR_FG_BLUE,
+    },
     // LAST
     {},
 };
