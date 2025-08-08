8cfee9d693feecb015a8c24a97c1a50fd2c19312
李通洲 <zhangsongcui3371@sina.com>
2025-08-08 10:05:20 +0800

    Netif: unifies BSD netif handling

--- src/common/netif/netif_bsd.c
+++ src/common/netif/netif_bsd.c
@@ -8,17 +8,24 @@
 #include <net/route.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/sysctl.h>
 
 #define ROUNDUP2(a, n)       ((a) > 0 ? (1 + (((a) - 1U) | ((n) - 1))) : (n))
 
-#if defined(__APPLE__) || defined(__FreeBSD__)
-# define ROUNDUP(a)           ROUNDUP2((a), sizeof(int))
-#elif defined(__NetBSD__)
-# define ROUNDUP(a)           ROUNDUP2((a), sizeof(uint64_t))
-#elif defined(__sun)
-# define ROUNDUP(a)           ROUNDUP2((a), _SS_ALIGNSIZE)
+#if __DragonFly__
+    // https://github.com/DragonFlyBSD/DragonFlyBSD/blob/cf0aa2f1e47a3f0a6055fe427563cb3f3e627064/sys/net/route.h#L315C9-L315C19
+    #define ROUNDUP(a)           ROUNDUP2((a), sizeof(long))
+#elif __FreeBSD__
+    // https://github.com/freebsd/freebsd-src/blob/e4c0ecba44b20ebb2e4d80978c2cb6d16b730cb9/sys/net/route.h#L368C9-L368C16
+    #define ROUNDUP(a)           ROUNDUP2((a), sizeof(long))
+#elif __NetBSD__
+    // https://github.com/NetBSD/src/blob/29beb637d057520c0ed37ac2cde966f7cc0cadf4/sys/net/route.h#L330
+    #define ROUNDUP(a)           ROUNDUP2((a), sizeof(uint64_t))
+#elif __OpenBSD__
+    // https://github.com/openbsd/src/blob/ca647cfa4ec3ccb8360714bc0ebc32a394f7fb6a/regress/sys/netinet/bindconnect/bindconnect.c#L250
+    #define ROUNDUP(a)           ROUNDUP2((a), sizeof(long))
 #else
-# error unknown platform
+    #error unknown platform
 #endif
 
 static struct sockaddr *
@@ -32,11 +39,7 @@ get_rt_address(struct rt_msghdr *rtm, int desired)
         {
             if ((1 << i) == desired)
                 return sa;
-#ifdef __sun
-            sa = (struct sockaddr *)(ROUNDUP(sizeof(struct sockaddr)) + (char *)sa);
-#else
             sa = (struct sockaddr *)(ROUNDUP(sa->sa_len) + (char *)sa);
-#endif
         }
     }
     return NULL;
@@ -44,55 +47,28 @@ get_rt_address(struct rt_msghdr *rtm, int desired)
 
 bool ffNetifGetDefaultRouteImplV4(FFNetifDefaultRouteResult* result)
 {
-    //https://github.com/hashPirate/copenheimer-masscan-fork/blob/36f1ed9f7b751a7dccd5ed27874e2e703db7d481/src/rawsock-getif.c#L104
+    int mib[6] = {CTL_NET, PF_ROUTE, 0, AF_INET, NET_RT_FLAGS, RTF_GATEWAY};
+    size_t needed;
 
-    FF_AUTO_CLOSE_FD int pfRoute = socket(PF_ROUTE, SOCK_RAW, AF_INET);
-    if (pfRoute < 0)
+    if (sysctl(mib, 6, NULL, &needed, NULL, 0) < 0 || needed == 0)
         return false;
 
-    {
-        struct timeval timeout = {1, 0};
-        setsockopt(pfRoute, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
-        setsockopt(pfRoute, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout));
-    }
+    FF_AUTO_FREE char* buf = malloc(needed);
 
-    int pid = getpid();
-
-    struct {
-        struct rt_msghdr hdr;
-        struct sockaddr_in dst;
-        uint8_t data[512];
-    } rtmsg = {
-        .hdr = {
-            .rtm_type = RTM_GET,
-            .rtm_flags = RTF_UP | RTF_GATEWAY,
-            .rtm_version = RTM_VERSION,
-            .rtm_addrs = RTA_DST | RTA_IFP | RTA_IFA,
-            .rtm_msglen = sizeof(rtmsg.hdr) + sizeof(rtmsg.dst),
-            .rtm_pid = pid,
-            .rtm_seq = 1,
-        },
-        .dst = {
-            .sin_family = AF_INET,
-            #ifndef __sun
-            .sin_len = sizeof(rtmsg.dst),
-            #endif
-        },
-    };
-
-    if (write(pfRoute, &rtmsg, rtmsg.hdr.rtm_msglen) != rtmsg.hdr.rtm_msglen)
+    if (sysctl(mib, 6, buf, &needed, NULL, 0) < 0)
         return false;
 
-    while (read(pfRoute, &rtmsg, sizeof(rtmsg)) > 0)
+    char* lim = buf + needed;
+    struct rt_msghdr* rtm;
+    for (char* next = buf; next < lim; next += rtm->rtm_msglen)
     {
-        if (rtmsg.hdr.rtm_seq == 1 && rtmsg.hdr.rtm_pid == pid)
+        rtm = (struct rt_msghdr *)next;
+        struct sockaddr* sa = (struct sockaddr *)(rtm + 1);
+
+        if ((rtm->rtm_flags & RTF_GATEWAY) && !(rtm->rtm_flags & RTF_REJECT) && (sa->sa_family == AF_INET))
         {
-            struct sockaddr_dl* sdl = (struct sockaddr_dl *)get_rt_address(&rtmsg.hdr, RTA_IFP);
-            if (sdl
-                #ifndef __sun
-                && sdl->sdl_len
-                #endif
-            )
+            struct sockaddr_dl* sdl = (struct sockaddr_dl *)get_rt_address(rtm, RTA_IFP);
+            if (sdl && sdl->sdl_family == AF_LINK)
             {
                 assert(sdl->sdl_nlen <= IF_NAMESIZE);
                 memcpy(result->ifName, sdl->sdl_data, sdl->sdl_nlen);
@@ -100,70 +76,41 @@ bool ffNetifGetDefaultRouteImplV4(FFNetifDefaultRouteResult* result)
                 result->ifIndex = sdl->sdl_index;
 
                 // Get the preferred source address
-                struct sockaddr_in* src = (struct sockaddr_in*)get_rt_address(&rtmsg.hdr, RTA_IFA);
+                struct sockaddr_in* src = (struct sockaddr_in*)get_rt_address(rtm, RTA_IFA);
                 if (src && src->sin_family == AF_INET)
                     result->preferredSourceAddrV4 = src->sin_addr.s_addr;
 
                 return true;
             }
-            return false;
         }
     }
-
     return false;
 }
 
 bool ffNetifGetDefaultRouteImplV6(FFNetifDefaultRouteResult* result)
 {
-    //https://github.com/hashPirate/copenheimer-masscan-fork/blob/36f1ed9f7b751a7dccd5ed27874e2e703db7d481/src/rawsock-getif.c#L104
+    int mib[6] = {CTL_NET, PF_ROUTE, 0, AF_INET6, NET_RT_FLAGS, RTF_GATEWAY};
+    size_t needed;
 
-    FF_AUTO_CLOSE_FD int pfRoute = socket(PF_ROUTE, SOCK_RAW, AF_INET6);
-    if (pfRoute < 0)
+    if (sysctl(mib, 6, NULL, &needed, NULL, 0) < 0 || needed == 0)
         return false;
 
-    {
-        struct timeval timeout = {1, 0};
-        setsockopt(pfRoute, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(timeout));
-        setsockopt(pfRoute, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout));
-    }
+    FF_AUTO_FREE char* buf = malloc(needed);
 
-    int pid = getpid();
-
-    struct {
-        struct rt_msghdr hdr;
-        struct sockaddr_in6 dst;
-        uint8_t data[512];
-    } rtmsg = {
-        .hdr = {
-            .rtm_type = RTM_GET,
-            .rtm_flags = RTF_UP | RTF_GATEWAY,
-            .rtm_version = RTM_VERSION,
-            .rtm_addrs = RTA_DST | RTA_IFP | RTA_IFA,
-            .rtm_msglen = sizeof(rtmsg.hdr) + sizeof(rtmsg.dst),
-            .rtm_pid = pid,
-            .rtm_seq = 1,
-        },
-        .dst = {
-            .sin6_family = AF_INET6,
-            #ifndef __sun
-            .sin6_len = sizeof(rtmsg.dst),
-            #endif
-        },
-    };
-
-    if (write(pfRoute, &rtmsg, rtmsg.hdr.rtm_msglen) != rtmsg.hdr.rtm_msglen)
+    if (sysctl(mib, 6, buf, &needed, NULL, 0) < 0)
         return false;
 
-    while (read(pfRoute, &rtmsg, sizeof(rtmsg)) > 0)
+    char* lim = buf + needed;
+    struct rt_msghdr* rtm;
+    for (char* next = buf; next < lim; next += rtm->rtm_msglen)
     {
-        if (rtmsg.hdr.rtm_seq == 1 && rtmsg.hdr.rtm_pid == pid)
+        rtm = (struct rt_msghdr *)next;
+        struct sockaddr* sa = (struct sockaddr *)(rtm + 1);
+
+        if ((rtm->rtm_flags & RTF_GATEWAY) && !(rtm->rtm_flags & RTF_REJECT) && (sa->sa_family == AF_INET6))
         {
-            struct sockaddr_dl* sdl = (struct sockaddr_dl *)get_rt_address(&rtmsg.hdr, RTA_IFP);
-            if (sdl
-                #ifndef __sun
-                && sdl->sdl_len
-                #endif
-            )
+            struct sockaddr_dl* sdl = (struct sockaddr_dl *)get_rt_address(rtm, RTA_IFP);
+            if (sdl && sdl->sdl_family == AF_LINK)
             {
                 assert(sdl->sdl_nlen <= IF_NAMESIZE);
                 memcpy(result->ifName, sdl->sdl_data, sdl->sdl_nlen);
@@ -172,7 +119,6 @@ bool ffNetifGetDefaultRouteImplV6(FFNetifDefaultRouteResult* result)
 
                 return true;
             }
-            return false;
         }
     }
 
