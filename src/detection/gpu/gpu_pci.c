24467e8c4676e2f7699ece15b646ab431b8d98a4
Carter Li <zhangsongcui3371@sina.com>
2025-08-21 09:18:16 +0800

    GPU (BSD): always use `pci.ids` provided by hwdata
    Fixes #1924

--- src/detection/gpu/gpu_pci.c
+++ src/detection/gpu/gpu_pci.c
@@ -45,9 +45,7 @@ static const FFstrbuf* loadPciIds()
             if (pciids.length == 0)
                 ffReadFileBuffer(FASTFETCH_TARGET_DIR_USR "/local/share/hwdata/pci.ids", &pciids);
         }
-        #elif __FreeBSD__ || __NetBSD__
-        ffReadFileBuffer(_PATH_LOCALBASE "/share/pciids/pci.ids", &pciids);
-        #elif __OpenBSD__
+        #elif __OpenBSD__ || __FreeBSD__ || __NetBSD__
         ffReadFileBuffer(_PATH_LOCALBASE "/share/hwdata/pci.ids", &pciids);
         #elif __sun
         ffReadFileBuffer(FASTFETCH_TARGET_DIR_ROOT "/usr/share/hwdata/pci.ids", &pciids);
