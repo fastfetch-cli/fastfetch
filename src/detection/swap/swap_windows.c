fastfetch-cli/fastfetch

commit 7ec0c664c541063dade5280c9e0efdde5ed6bdcf

Swap (Windows): detect detailed info

src/detection/swap/swap_windows.c
@@ -1,11 +1,12 @@
 #include "swap.h"
 #include "util/mallocHelper.h"
+#include "util/windows/unicode.h"
 
 #include <winternl.h>
 #include <ntstatus.h>
 #include <windows.h>
 
-const char* ffDetectSwap(FFSwapResult* swap)
+const char* ffDetectSwap(FFlist* result)
 {
     uint8_t buffer[4096];
     ULONG size = sizeof(buffer);
@@ -13,16 +14,18 @@ const char* ffDetectSwap(FFSwapResult* swap)
     if(!NT_SUCCESS(NtQuerySystemInformation(SystemPagefileInformation, pstart, size, &size)))
         return "NtQuerySystemInformation(SystemPagefileInformation, size) failed";
 
+    uint32_t pageSize = instance.state.platform.sysinfo.pageSize;
     for (SYSTEM_PAGEFILE_INFORMATION* current = pstart; ; current = (SYSTEM_PAGEFILE_INFORMATION*)((uint8_t*) current + current->NextEntryOffset))
     {
-        swap->bytesUsed += current->TotalUsed;
-        swap->bytesTotal += current->CurrentSize;
+        FFSwapResult* swap = ffListAdd(result);
+        ffStrbufInitNWS(&swap->name, current->FileName.Length / sizeof(wchar_t), current->FileName.Buffer);
+        if (ffStrbufStartsWithS(&swap->name, "\\??\\"))
+            ffStrbufSubstrAfter(&swap->name, strlen("\\??\\") - 1);
+        swap->bytesUsed = current->TotalUsed * pageSize;
+        swap->bytesTotal = current->CurrentSize * pageSize;
         if (current->NextEntryOffset == 0)
             break;
     }
-    uint32_t pageSize = instance.state.platform.sysinfo.pageSize;
-    swap->bytesUsed *= pageSize;
-    swap->bytesTotal *= pageSize;
 
     return NULL;
 }

src/detection/swap/swap.h
@@ -4,8 +4,9 @@
 
 typedef struct FFSwapResult
 {
+    FFstrbuf name;
     uint64_t bytesUsed;
     uint64_t bytesTotal;
 } FFSwapResult;
 
-const char* ffDetectSwap(FFSwapResult* swap);
+const char* ffDetectSwap(FFlist* result /* List of FFSwapResult */);

commit c10b652a2e3933b0cc69f4f31ec4e99329816428

Swap (Windows): prevents integer overflow in swap size
Regression from 7ec0c664c541063dade5280c9e0efdde5ed6bdcf

src/detection/swap/swap_windows.c
@@ -21,8 +21,8 @@ const char* ffDetectSwap(FFlist* result)
         ffStrbufInitNWS(&swap->name, current->FileName.Length / sizeof(wchar_t), current->FileName.Buffer);
         if (ffStrbufStartsWithS(&swap->name, "\\??\\"))
             ffStrbufSubstrAfter(&swap->name, strlen("\\??\\") - 1);
-        swap->bytesUsed = current->TotalUsed * pageSize;
-        swap->bytesTotal = current->CurrentSize * pageSize;
+        swap->bytesUsed = (uint64_t) current->TotalUsed * pageSize;
+        swap->bytesTotal = (uint64_t) current->CurrentSize * pageSize;
         if (current->NextEntryOffset == 0)
             break;
     }