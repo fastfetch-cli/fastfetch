c324f838262426252d44e6b87d8352c5949ad427
Carter Li <zhangsongcui3371@sina.com>
2025-12-08 09:03:28 +0800

    Logo (Builtin): drop opak
    Ref: #1070

--- src/logo/builtin.c
+++ src/logo/builtin.c
@@ -3455,12 +3455,6 @@ static const FFlogo O[] = {
             FF_COLOR_FG_LIGHT_BLACK,
         }
     },
-    // Opak
-    {
-        .names = {"Opak"},
-        .lines = FASTFETCH_DATATEXT_LOGO_OPAK,
-        .colors = {}, // #1070
-    },
     // OpenKylin
     {
         .names = {"openkylin", "open-kylin"},
