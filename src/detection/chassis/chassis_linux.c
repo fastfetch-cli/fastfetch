0f3db37199776402c2fa8758cbe2d1bc341e5d56
Carter Li <zhangsongcui3371@sina.com>
2026-01-16 16:47:51 +0800

    Chassis (Linux): supports arm devices

--- src/detection/chassis/chassis_linux.c
+++ src/detection/chassis/chassis_linux.c
@@ -2,20 +2,28 @@
 #include "common/io.h"
 #include "common/smbiosHelper.h"
 
-#include <stdlib.h>
+#include <ctype.h>
 
 const char* ffDetectChassis(FFChassisResult* result)
 {
-    ffGetSmbiosValue("/sys/devices/virtual/dmi/id/chassis_type", "/sys/class/dmi/id/chassis_type", &result->type);
-    ffGetSmbiosValue("/sys/devices/virtual/dmi/id/chassis_serial", "/sys/class/dmi/id/chassis_serial", &result->serial);
-    ffGetSmbiosValue("/sys/devices/virtual/dmi/id/chassis_vendor", "/sys/class/dmi/id/chassis_vendor", &result->vendor);
-    ffGetSmbiosValue("/sys/devices/virtual/dmi/id/chassis_version", "/sys/class/dmi/id/chassis_version", &result->version);
-
-    if(result->type.length)
+    if (ffGetSmbiosValue("/sys/devices/virtual/dmi/id/chassis_type", "/sys/class/dmi/id/chassis_type", &result->type))
     {
-        const char* typeStr = ffChassisTypeToString((uint32_t) ffStrbufToUInt(&result->type, 9999));
-        if(typeStr)
-            ffStrbufSetS(&result->type, typeStr);
+        ffGetSmbiosValue("/sys/devices/virtual/dmi/id/chassis_serial", "/sys/class/dmi/id/chassis_serial", &result->serial);
+        ffGetSmbiosValue("/sys/devices/virtual/dmi/id/chassis_vendor", "/sys/class/dmi/id/chassis_vendor", &result->vendor);
+        ffGetSmbiosValue("/sys/devices/virtual/dmi/id/chassis_version", "/sys/class/dmi/id/chassis_version", &result->version);
+
+        if(result->type.length)
+        {
+            const char* typeStr = ffChassisTypeToString((uint32_t) ffStrbufToUInt(&result->type, 9999));
+            if(typeStr)
+                ffStrbufSetS(&result->type, typeStr);
+        }
+    }
+
+    else if(ffReadFileBuffer("/proc/device-tree/chassis-type", &result->type) && result->type.length > 0)
+    {
+        ffStrbufTrimRight(&result->type, '\0');
+        result->type.chars[0] = (char) toupper(result->type.chars[0]);
     }
     return NULL;
 }
