6553873b5f4dc44c44d62a1ac8c44c520caaa619
biarder-dev <mss091204.1@gmail.com>
2026-03-26 01:44:52 +0700

    feat(bsd): exclude reclaimable ARC usage from used memory

--- src/detection/memory/memory_bsd.c
+++ src/detection/memory/memory_bsd.c
@@ -7,10 +7,17 @@ const char* ffDetectMemory(FFMemoryResult* ram)
     if (sysctl((int[]){ CTL_HW, HW_PHYSMEM }, 2, &ram->bytesTotal, &length, NULL, 0))
         return "Failed to read hw.physmem";
 
+    // calculates the reclaimable ARC pages.
+    int64_t arcSize = ffSysctlGetInt64("kstat.zfs.misc.arcstats.size", 0);
+    int64_t arcMin = ffSysctlGetInt64("vfs.zfs.arc_min", 0);
+    int64_t arcPages = (arcSize > arcMin) ? (arcSize - arcMin)
+        / instance.state.platform.sysinfo.pageSize : 0;
+
     // vm.stats.vm.* are int values
     int32_t pagesFree = ffSysctlGetInt("vm.stats.vm.v_free_count", 0)
         + ffSysctlGetInt("vm.stats.vm.v_inactive_count", 0)
-        + ffSysctlGetInt("vm.stats.vm.v_cache_count", 0);
+        + ffSysctlGetInt("vm.stats.vm.v_cache_count", 0)
+        + (int32_t) arcPages;
 
     ram->bytesUsed = ram->bytesTotal - (uint64_t) pagesFree * instance.state.platform.sysinfo.pageSize;
 
