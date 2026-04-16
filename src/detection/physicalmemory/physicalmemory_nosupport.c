1956c66a5ea149182a4f2d23e374b86665834e76
李通洲 <zhangsongcui3371@sina.com>
2026-04-16 22:42:59 +0800

    PhysicalMemory (Android): fixes compiling

--- src/detection/physicalmemory/physicalmemory_nosupport.c
+++ src/detection/physicalmemory/physicalmemory_nosupport.c
@@ -1,5 +1,6 @@
 #include "physicalmemory.h"
 
-const char* ffDetectPhysicalMemory(FF_A_UNUSED FFlist* result) {
+const char* ffDetectPhysicalMemory(FFPhysicalMemoryOptions* options, FFlist* result) {
+    FF_UNUSED(options, result);
     return "Not supported on this platform";
 }
