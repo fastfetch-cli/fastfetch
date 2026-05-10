b788495ca80ed27ebf680bd48af25afc218a0770
François Revol <revol@free.fr>
2026-05-10 21:59:01 +0200

    Update nosupport stubs so they compile

--- src/detection/media/media_nosupport.c
+++ src/detection/media/media_nosupport.c
@@ -1,5 +1,5 @@
 #include "media.h"
 
-void ffDetectMediaImpl(FFMediaResult* media) {
+void ffDetectMediaImpl(FFMediaResult* media, bool saveCover) {
     ffStrbufAppendS(&media->error, "Not supported on this platform");
 }
