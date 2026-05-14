36afc9103bf4e38ae4c1498c6243a50f33337bfc
François Revol <revol@free.fr>
2026-05-14 23:47:27 +0200

    BIOS (MiNT): implement

--- src/detection/bios/bios_mint.c
+++ src/detection/bios/bios_mint.c
@@ -4,13 +4,22 @@
 #include "common/io.h"
 
 #include <mint/sysvars.h>
+#include <mint/osbind.h>
 
 const char* ffDetectBios(FFBiosResult* bios) {
-    OSHEADER *oh = (OSHEADER *)_sysbase;
+    OSHEADER *oh;
+    unsigned long oldmode = Super(0L);
+    oh    = *(OSHEADER **)_sysbase;
+    Super(oldmode);
 
-    ffStrbufSetF(&bios->version, "%d", oh->os_version);
+    ffStrbufSetF(&bios->version, "%2x.%02x", (oh->os_version >> 8), oh->os_version & 0x0ff);
     unsigned long d = (unsigned) oh->os_date;
-    ffStrbufSetF(&bios->date, "%04d-%02d-%02d", (d & 0x0ffff), ((d >> 24) & 0x0ff), ((d >> 16) & 0x0ff));
+    // Seems like EmuTOS doesn't agree with TOS on the date format?
+    unsigned long y = (d >> 16) & 0x0ffff;
+    if ((y >= 0x1980) && (y < 0x2100))
+        ffStrbufSetF(&bios->date, "%04x-%02x-%02x", ((d >> 16) & 0x0ffff), ((d >> 8) & 0x0ff), (d & 0x0ff));
+    else
+        ffStrbufSetF(&bios->date, "%04x-%02x-%02x", (d & 0x0ffff), ((d >> 24) & 0x0ff), ((d >> 16) & 0x0ff));
     ffStrbufSetStatic(&bios->type, "BIOS");
 
     return NULL;
