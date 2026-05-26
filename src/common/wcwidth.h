173d9695b6e444b37febe97052178f44f9013138
Carter Li <zhangsongcui@live.cn>
2026-05-26 16:10:01 +0800

    Common: removes using of built-in wcwidth, which is locale-dependent

--- src/common/wcwidth.h
+++ src/common/wcwidth.h
@@ -2,15 +2,4 @@
 
 #include <stdint.h>
 
-#ifdef FF_HAVE_WCWIDTH
-    #include <wchar.h>
-
-// Should be char32_t but it's not defined on macOS
-static_assert(sizeof(wchar_t) == sizeof(uint32_t), "wcwidth implementation requires wchar_t to be 32 bits");
-
-static inline int mk_wcwidth(uint32_t ucs) {
-    return wcwidth((wchar_t) ucs);
-}
-#else
 int mk_wcwidth(uint32_t wc);
-#endif
