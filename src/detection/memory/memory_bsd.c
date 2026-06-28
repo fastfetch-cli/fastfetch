e0d141ecf6d89fe3ee8229d84be450376fedc946
李通洲 <zhangsongcui3371@sina.com>
2026-06-28 15:24:03 +0800

    Memory (BSD): adds underflow protection (should not happen)

--- src/detection/memory/memory_bsd.c
+++ src/detection/memory/memory_bsd.c
@@ -11,11 +11,13 @@ const char* ffDetectMemory(FFMemoryResult* ram) {
     int32_t pagesFree = ffSysctlGetInt("vm.stats.vm.v_free_count", 0) + ffSysctlGetInt("vm.stats.vm.v_inactive_count", 0) + ffSysctlGetInt("vm.stats.vm.v_cache_count", 0);
     int64_t bytesCache = ffSysctlGetInt64("vfs.bufspace", 0);
 
+    ram->bytesUsed = ram->bytesTotal - (uint64_t) pagesFree * instance.state.platform.sysinfo.pageSize - (uint64_t) bytesCache;
+
     int64_t bytesArc = ffSysctlGetInt64("kstat.zfs.misc.arcstats.size", 0);
     if (bytesArc > 0) bytesArc -= ffSysctlGetInt64("kstat.zfs.misc.arcstats.c_min", 0);
-    if (bytesArc < 0) bytesArc = 0;
-
-    ram->bytesUsed = ram->bytesTotal - (uint64_t) pagesFree * instance.state.platform.sysinfo.pageSize - (uint64_t) bytesCache - (uint64_t) bytesArc;
+    if (bytesArc > 0 && (uint64_t) bytesArc < ram->bytesUsed) {
+        ram->bytesUsed -= (uint64_t) bytesArc;
+    }
 
     return NULL;
 }
