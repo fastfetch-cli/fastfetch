bc76a3d31b0a9103fa40aff55bd8248b9e385396
Carter Li <CarterLi@users.noreply.github.com>
2026-07-05 21:58:00 -0500

    Refactor pkgsrc package path retrieval logic

--- src/detection/packages/packages_pkgsrc.c
+++ src/detection/packages/packages_pkgsrc.c
@@ -1,5 +1,4 @@
 #include "packages.h"
-#include "common/io.h"
 #include "common/path.h"
 #include "common/properties.h"
 
@@ -8,27 +7,28 @@ uint32_t ffPackagesGetPkgsrc(FFstrbuf* baseDir) {
     FF_STRBUF_AUTO_DESTROY pkginPath = ffStrbufCreate();
 
     if (ffFindExecutableInPath("pkgin", &pkginPath) != NULL) {
-        ffStrbufSubstrBefore(baseDir, baseDirLength);
         return 0;
     }
 
     ffStrbufSubstrBeforeLastC(&pkginPath, '/'); // remove /pkgin
     ffStrbufSubstrBeforeLastC(&pkginPath, '/'); // remove /bin
+    uint32_t pkginPathLength = pkginPath.length;
 
-    FF_STRBUF_AUTO_DESTROY mkconf = ffStrbufCreateCopy(&pkginPath);
-    ffStrbufAppendS(&mkconf, "/etc/mk.conf");
+    ffStrbufAppendS(&pkginPath, "/etc/mk.conf");
 
     FF_STRBUF_AUTO_DESTROY dbdir = ffStrbufCreate();
-    ffParsePropFile(mkconf.chars, "PKG_DBDIR=", &dbdir);
+    ffParsePropFile(pkginPath.chars, "PKG_DBDIR=", &dbdir);
+    ffStrbufSubstrBefore(&pkginPath, pkginPathLength); // remove /etc/mk.conf
 
     if (dbdir.length > 0) {
-        ffStrbufAppendS(baseDir, dbdir.chars);
+        ffStrbufAppend(baseDir, &dbdir);
     } else {
-        ffStrbufAppendF(baseDir, "%s/pkgdb", pkginPath.chars);
+        ffStrbufAppend(baseDir, &pkginPath);
+        ffStrbufAppendS(baseDir, "/pkgdb");
     }
 
     uint32_t result = ffPackagesGetNumElements(baseDir->chars, true);
     ffStrbufSubstrBefore(baseDir, baseDirLength);
-    
+
     return result;
 }
