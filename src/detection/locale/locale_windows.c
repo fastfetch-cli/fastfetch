1d120035557c04002fbb5660e43e6175dbfa3871
李通洲 <zhangsongcui3371@sina.com>
2026-08-12 10:19:22 +0800

    Locale (Windows): uses $LANG if available e.g. MSYS2

--- src/detection/locale/locale_windows.c
+++ src/detection/locale/locale_windows.c
@@ -1,9 +1,19 @@
 #include "detection/locale/locale.h"
 #include "common/windows/unicode.h"
 
-#include <windows.h>
+#include <winnls.h>
 
 const char* ffDetectLocale(FFstrbuf* result) {
+    ffStrbufAppendS(result, getenv("LC_ALL"));
+    if (result->length > 0) {
+        return nullptr;
+    }
+
+    ffStrbufAppendS(result, getenv("LANG")); // Available in MSYS2 and Cygwin
+    if (result->length > 0) {
+        return nullptr;
+    }
+
     wchar_t name[LOCALE_NAME_MAX_LENGTH];
     int size = GetUserDefaultLocaleName(name, LOCALE_NAME_MAX_LENGTH);
     if (size <= 1) { // including '\0'
