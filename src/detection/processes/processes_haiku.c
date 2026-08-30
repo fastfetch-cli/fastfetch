39024abae958ff0cef8a5bd7ab83740ba3e61148
李通洲 <zhangsongcui3371@sina.com>
2026-08-30 19:41:18 +0800

    Processes (Haiku): honors `options->countKprocs`

--- src/detection/processes/processes_haiku.c
+++ src/detection/processes/processes_haiku.c
@@ -2,7 +2,7 @@
 
 #include <OS.h>
 
-const char* ffDetectProcesses(const FFProcessesOptions*, FFProcessesResult* result) {
+const char* ffDetectProcesses(const FFProcessesOptions* options, FFProcessesResult* result) {
     system_info info;
     if (get_system_info(&info) != B_OK) {
         return "Error getting system info";
@@ -10,6 +10,14 @@ const char* ffDetectProcesses(const FFProcessesOptions*, FFProcessesResult* resu
 
     result->processes = info.used_teams;
     result->threads = info.used_threads;
+
+    if (!options->countKprocs) {
+        team_info ti;
+        if (get_team_info(B_SYSTEM_TEAM, &ti) == B_OK) {
+            result->processes--;
+            result->threads -= (uint32_t) ti.thread_count;
+        }
+    }
 
     return nullptr;
 }
